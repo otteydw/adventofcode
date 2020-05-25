b4f7c20043bbd55a2b4431b0052ec15e712ddc77
Dan Ottey <dottey@boomi.com>
2020-05-25 08:18:02 -0400

    Complete Day2 part 2

--- day2/day2b.py
+++ day2/day2b.py
@@ -1,113 +1,63 @@
 #!/usr/bin/python3
 
-# https://adventofcode.com/2019/day/2
+import sys
 
-# --- Day 2: 1202 Program Alarm --- On the way to your gravity assist around the
-# Moon, your ship computer beeps angrily about a "1202 program alarm". On the
-# radio, an Elf is already explaining how to handle the situation: "Don't worry,
-# that's perfectly norma--" The ship computer bursts into flames.
+def get_opcode(program, position):
+    return program[position]
 
-# You notify the Elves that the computer's magic smoke seems to have escaped.
-# "That computer ran Intcode programs like the gravity assist program it was
-# working on; surely there are enough spare parts up there to build a new
-# Intcode computer!"
+def intcode(program, position=0):
 
-# An Intcode program is a list of integers separated by commas (like
-# 1,0,0,3,99). To run one, start by looking at the first integer (called
-# position 0). Here, you will find an opcode - either 1, 2, or 99. The opcode
-# indicates what to do; for example, 99 means that the program is finished and
-# should immediately halt. Encountering an unknown opcode means something went
-# wrong.
+    opcode = get_opcode(program, position)
 
-# Opcode 1 adds together numbers read from two positions and stores the result
-# in a third position. The three integers immediately after the opcode tell you
-# these three positions - the first two indicate the positions from which you
-# should read the input values, and the third indicates the position at which
-# the output should be stored.
+    if opcode == 1:
+        opcode_add(program, position)
 
-# For example, if your Intcode computer encounters 1,10,20,30, it should read
-# the values at positions 10 and 20, add those values, and then overwrite the
-# value at position 30 with their sum.
+    elif opcode == 2:
+        opcode_multiply(program, position)
 
-# Opcode 2 works exactly like opcode 1, except it multiplies the two inputs
-# instead of adding them. Again, the three integers after the opcode indicate
-# where the inputs and outputs are, not their values.
+    elif opcode == 99:
+        return program
 
-# Once you're done processing an opcode, move to the next one by stepping
-# forward 4 positions.
+    else:
+        print('Invalid opcode!')
+        sys.exit(1)
 
-# For example, suppose you have the following program:
+    position += 4
+    return intcode(program, position)
 
-# 1,9,10,3,2,3,11,0,99,30,40,50 For the purposes of illustration, here is the
-# same program split into multiple lines:
+def opcode_add(program, position):
+    storage_position = program[position + 3]
+    a_pos = program[position + 1]
+    b_pos = program[position + 2]
+    a_val = program[a_pos]
+    b_val = program[b_pos]
+    program[storage_position] = a_val + b_val
+    return program
 
-# 1,9,10,3, 2,3,11,0, 99, 30,40,50 The first four integers, 1,9,10,3, are at
-# positions 0, 1, 2, and 3. Together, they represent the first opcode (1,
-# addition), the positions of the two inputs (9 and 10), and the position of the
-# output (3). To handle this opcode, you first need to get the values at the
-# input positions: position 9 contains 30, and position 10 contains 40. Add
-# these numbers together to get 70. Then, store this value at the output
-# position; here, the output position (3) is at position 3, so it overwrites
-# itself. Afterward, the program looks like this:
+def opcode_multiply(program, position):
+    storage_position = program[position + 3]
+    a_pos = program[position + 1]
+    b_pos = program[position + 2]
+    a_val = program[a_pos]
+    b_val = program[b_pos]
+    program[storage_position] = a_val * b_val
+    return program
 
-# 1,9,10,70, 2,3,11,0, 99, 30,40,50 Step forward 4 positions to reach the next
-# opcode, 2. This opcode works just like the previous, but it multiplies instead
-# of adding. The inputs are at positions 3 and 11; these positions contain 70
-# and 50 respectively. Multiplying these produces 3500; this is stored at
-# position 0:
-
-# 3500,9,10,70, 2,3,11,0, 99, 30,40,50 Stepping forward 4 more positions arrives
-# at opcode 99, halting the program.
-
-# Here are the initial and final states of a few more small programs:
-
-# 1,0,0,0,99 becomes 2,0,0,0,99 (1 + 1 = 2). 2,3,0,3,99 becomes 2,3,0,6,99 (3 *
-# 2 = 6). 2,4,4,5,99,0 becomes 2,4,4,5,99,9801 (99 * 99 = 9801).
-# 1,1,1,4,99,5,6,0,99 becomes 30,1,1,4,2,5,6,0,99. Once you have a working
-# computer, the first step is to restore the gravity assist program (your puzzle
-# input) to the "1202 program alarm" state it had just before the last computer
-# caught fire. To do this, before running the program, replace position 1 with
-# the value 12 and replace position 2 with the value 2. What value is left at
-# position 0 after the program halts?
-
-def intcode(input_list):
-
-    for index in range(int((len(input_list))/4)):
-        minicode = input_list[index*4:(index*4)+4]
-        opcode = int(minicode[0])
-
-        if opcode == 99:
-            break
-        else:
-            operand1 = int(input_list[int(minicode[1])])
-            operand2 = int(input_list[int(minicode[2])])
-            result_position = int(minicode[3])
-
-            if opcode == 1:
-                # Opcode 1 adds together numbers read from two positions and stores the result in a third position
-                input_list[result_position] = operand1 + operand2
-            elif opcode == 2:
-                # Opcode 2 works exactly like opcode 1, except it multiplies the two inputs instead of adding them.
-                input_list[result_position] = operand1 * operand2
-            else:
-                print('Unknown op code!')
-        # print()
-
-    return input_list
 
 inputs_path = 'input.txt'
-DESIRED_OUTPUT = 19690720
 
 with open(inputs_path) as input_file:
-    original_input_program = [int(x) for x in input_file.readline().rstrip().split(',')]
+    input_program = [int(x) for x in input_file.readline().rstrip().split(',')]
 
+
+DESIRED_OUTPUT = 19690720
 for noun in range(0, 99):
     for verb in range(0, 99):
-        input_program = original_input_program.copy()
-        input_program[1] = noun
-        input_program[2] = verb
+        this_program = input_program.copy()
+        this_program[1] = noun
+        this_program[2] = verb
 
-        if intcode(input_program)[0] == DESIRED_OUTPUT:
+        if intcode(this_program)[0] == DESIRED_OUTPUT:
             print('Noun = ' + str(noun))
             print('Verb = ' + str(verb))
             print('Answer = ' + str(100 * noun + verb))
