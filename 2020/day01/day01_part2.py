3370be69b8e594204ba81179d06a11dd18681121
Dan Ottey <dottey@boomi.com>
2021-01-11 14:12:44 -0500

    day1 part 2

---
+++ 2020/day01/day01_part2.py
@@ -0,0 +1,17 @@
+
+# https://adventofcode.com/2020/day/1
+
+inputs_path = 'input.txt'
+
+with open(inputs_path) as inputs_file:
+    entries = inputs_file.readlines()
+entries = [int(x.strip()) for x in entries]
+
+# print(entries)
+
+for a in entries:
+    for b in entries:
+        for c in entries:
+            if a + b + c == 2020:
+                print(a * b * c)
+                exit()
