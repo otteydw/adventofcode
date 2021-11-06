16fcc1f2aad0f65e1b93f83046170bdbef708690
Dan Ottey <dottey@boomi.com>
2021-11-05 22:20:20 -0400

    Completed day02

--- 2015/day02/day02.py
+++ 2015/day02/day02.py
@@ -15,16 +15,31 @@ class Present():
     def paper_needed(self):
         return self.surface_area() + self.smallest_side_area()
 
+    def smallest_perimeter(self):
+        dimensions = [self.length, self.width, self.height]
+        dimensions.sort()
+        smallest_perimeter = 2 * dimensions[0] + 2 * dimensions[1]
+        return smallest_perimeter
+
+    def volume(self):
+        return self.length * self.width * self.height
+
+    def ribbon_needed(self):
+        return self.smallest_perimeter() + self.volume()
+
 if __name__ == "__main__":
 
     input_path = os.path.join(os.path.dirname(__file__),"input.txt")
 
     total_paper = 0
+    total_ribbon = 0
 
     with open(input_path) as input_file:
         for line in input_file:
             dimension = line.rstrip()
             present = Present(dimension)
             total_paper += present.paper_needed()
+            total_ribbon += present.ribbon_needed()
 
-    print(total_paper)
+    print(f"Paper: {total_paper}")
+    print(f"Ribbon: {total_ribbon}")
--- 2015/day02/test_day02.py
+++ 2015/day02/test_day02.py
@@ -5,8 +5,10 @@ class TestPresent(unittest.TestCase):
     def test_present_paper(self):
         present = Present("2x3x4")
         self.assertAlmostEqual(present.paper_needed(), 58)
+        self.assertAlmostEqual(present.ribbon_needed(), 34)
         present = Present("1x1x10")
         self.assertAlmostEqual(present.paper_needed(), 43)
+        self.assertAlmostEqual(present.ribbon_needed(), 14)
 
 if __name__ == '__main__':
     unittest.main()
