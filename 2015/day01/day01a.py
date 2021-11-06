37dd205b828776daaf18abdc5ef3b2399658544a
Dan Ottey <dottey@boomi.com>
2021-11-05 20:57:37 -0400

    2015 Day01a complete with tests.

---
+++ 2015/day01/day01a.py
@@ -0,0 +1,21 @@
+
+
+def find_floor(directions):
+    floor = 0
+
+    for direction in directions:
+        if direction == "(":
+            floor += 1
+        elif direction == ")":
+            floor -=1
+
+    return floor
+
+if __name__ == "__main__":
+
+    input_path = "2015/day01/input.txt"
+
+    with open(input_path) as input_file:
+        INPUT = input_file.readline()
+
+    print(find_floor(INPUT))
