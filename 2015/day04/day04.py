c0f1998980f4433c22a6b76fb83f7774b46e0f72
Dan Ottey <dottey@boomi.com>
2021-11-06 22:34:31 -0400

    2015 Day 4 Part 2 complete.

--- 2015/day04/day04.py
+++ 2015/day04/day04.py
@@ -8,17 +8,17 @@ def combine_secret_and_number(secret_key, number):
 def get_hash(string):
     return hashlib.md5(string.encode()).hexdigest()
 
-def is_mineable(hash):
-    return hash[:5] == "00000"
+def is_mineable(hash, num_zeroes=5):
+    return hash[:num_zeroes] == "0" * num_zeroes
 
-def find_answer(secret_key):
+def find_answer(secret_key, num_zeroes=5):
 
     counter = 0
 
     while True:
         str2hash = combine_secret_and_number(secret_key, counter)
         md5 = get_hash(str2hash)
-        if is_mineable(md5):
+        if is_mineable(md5, num_zeroes):
             return counter
         counter += 1
 
@@ -26,4 +26,5 @@ if __name__ == "__main__":
 
     SECRET = "yzbqklnj"
 
-    print(find_answer(SECRET))
+    print(f"5 zeroes: {find_answer(SECRET)}")
+    print(f"6 zeroes: {find_answer(SECRET, 6)}")
