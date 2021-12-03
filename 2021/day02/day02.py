bad0551290074a35218988e099f7bc7d25510ed6
Dan Ottey <dottey@boomi.com>
2021-12-03 07:55:39 -0500

    Day 02 Part 2 complete.

--- 2021/day02/day02.py
+++ 2021/day02/day02.py
@@ -19,6 +19,26 @@ class Submarine():
         else:
             raise Exception(f'Invalid direction {direction}!')
 
+class Submarine2():
+    def __init__(self):
+        self.depth = 0
+        self.horizontal_position = 0
+        self.aim = 0
+
+    def move(self, instruction):
+        direction, distance = instruction.split(' ')
+        distance = int(distance)
+
+        if direction == "forward":
+            self.horizontal_position += distance
+            self.depth += self.aim * distance
+        elif direction == "down":
+            self.aim += distance
+        elif direction == "up":
+            self.aim -= distance
+        else:
+            raise Exception(f'Invalid direction {direction}!')
+
 def load_from_file(filename):
     input_path = os.path.join(os.path.dirname(__file__),filename)
 
@@ -30,9 +50,16 @@ def load_from_file(filename):
 
 if __name__ == "__main__":
 
-    sub = Submarine()
     instructions = load_from_file("input.txt")
+
+    sub = Submarine()
     for instruction in instructions:
         sub.move(instruction)
 
     print(f'Part 1 answer is: {sub.depth * sub.horizontal_position}')
+
+    sub = Submarine2()
+    for instruction in instructions:
+        sub.move(instruction)
+
+    print(f'Part 2 answer is: {sub.depth * sub.horizontal_position}')
