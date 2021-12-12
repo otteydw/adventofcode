88431e9b43ca4c5ce37e8260e4b5fb9839dc9e4b
Dan Ottey <dottey@boomi.com>
2021-12-12 09:05:48 -0500

    2021 Day 06 Part 2 complete.

--- 2021/day06/day06.py
+++ 2021/day06/day06.py
@@ -1,4 +1,6 @@
 import os
+from collections import defaultdict
+
 
 class LanternFish:
     def __init__(self, internal_timer):
@@ -37,7 +39,7 @@ def load_lanternfish_data(filename):
 
 
 def number_of_lanternfish_orig(lanternfish_data, days):
-    # Original version using the LanternFish class
+    # Original version I wrote using the LanternFish class
 
     fishes = []
     for initial_fish_timer in lanternfish_data:
@@ -58,14 +60,24 @@ def number_of_lanternfish_orig(lanternfish_data, days):
 
     return len(fishes)
 
+
 def number_of_lanternfish_unclassed(fish_timers, days):
-    # Un-classed version is faster, but still not efficient.
-    # Ref: https://github.com/womogenes/AoC-2021-Solutions/tree/main/day_06
-    # https://www.youtube.com/watch?v=yJjpXJm7x0o
+    """ Unclassed version
+
+    The version that used the LanternFish class worked for part 1 but was not efficient for part 2 (256 days).
+
+    I went online for some advice and found this video https://www.youtube.com/watch?v=yJjpXJm7x0o
+
+    It seems the most efficient method for doing this would need to not care about each individual fish, but instead the
+    frequency of each fish' starting timer.
+
+    I rewrote my function to remove the LaternFish class, somewhat based on code from
+    https://github.com/womogenes/AoC-2021-Solutions/blob/main/day_06/day_06_p1.py
+
+    """
 
     for day in range(days):
         print(f"At day {day} there are {len(fish_timers)} fish.")
-        # print(fishes)
 
         next_fish_timers = []
         for fish_timer in fish_timers:
@@ -78,7 +90,42 @@ def number_of_lanternfish_unclassed(fish_timers, days):
 
     return len(fish_timers)
 
+
+def number_of_lanternfish_counters(fish_timers, days):
+    """ Frequency Counting version
+
+    After I removed the class (above) it was still inefficient for part 2 (256 days).
+
+    The advice from https://github.com/womogenes/AoC-2021-Solutions/blob/main/day_06/day_06_p2.py would be to use a frequency counter.
+
+    I admit the following code looks almost identical to the above linked code, but I at least learned how to use the defaultdict.
+
+    """
+
+    frequencies = defaultdict(int)
+    for fish_timer in fish_timers:
+        frequencies[fish_timer] += 1
+
+    for _ in range(days):
+        new_frequencies = defaultdict(int)
+
+        for fish_timer in frequencies:
+            if fish_timer == 0:
+                new_frequencies[6] += frequencies[fish_timer]
+                new_frequencies[8] = frequencies[fish_timer]
+            else:
+                new_frequencies[fish_timer - 1] += frequencies[fish_timer]
+        frequencies = new_frequencies
+
+    sum_of_fish = 0
+    for frequency in frequencies:
+        sum_of_fish += frequencies[frequency]
+
+    return sum_of_fish
+
+
 if __name__ == "__main__":
 
     lanternfish_data = load_lanternfish_data("input.txt")
-    print(number_of_lanternfish(lanternfish_data, 80))
+    print(number_of_lanternfish_counters(lanternfish_data, 80))
+    print(number_of_lanternfish_counters(lanternfish_data, 256))
--- 2021/day06/test_day06.py
+++ 2021/day06/test_day06.py
@@ -14,9 +14,24 @@ class TestSanta(unittest.TestCase):
     def test_lanternfish_unclassed(self):
 
         lanternfish_data = day06.load_lanternfish_data("example.txt")
-        self.assertEqual(day06.number_of_lanternfish_unclassed(lanternfish_data, 18), 26)
-        self.assertEqual(day06.number_of_lanternfish_unclassed(lanternfish_data, 80), 5934)
-        # self.assertEqual(day06.number_of_lanternfish_unclassed(lanternfish_data, 256), 26984457539)
+        self.assertEqual(
+            day06.number_of_lanternfish_unclassed(lanternfish_data, 18), 26
+        )
+        self.assertEqual(
+            day06.number_of_lanternfish_unclassed(lanternfish_data, 80), 5934
+        )
+
+    def test_lanternfish_counters(self):
+
+        lanternfish_data = day06.load_lanternfish_data("example.txt")
+        self.assertEqual(day06.number_of_lanternfish_counters(lanternfish_data, 18), 26)
+        self.assertEqual(
+            day06.number_of_lanternfish_counters(lanternfish_data, 80), 5934
+        )
+        self.assertEqual(
+            day06.number_of_lanternfish_counters(lanternfish_data, 256), 26984457539
+        )
+
 
 if __name__ == "__main__":
     unittest.main()
