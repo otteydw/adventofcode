7cec5de11bd0ed71d4fcacf338088d04ff0fa635
Dan Ottey <dottey@boomi.com>
2021-12-23 17:20:21 -0500

    2021 Day 9 Cleanup and Code comments.

--- 2021/day09/day09_part2.py
+++ 2021/day09/day09_part2.py
@@ -6,10 +6,13 @@ from scipy import ndimage
 class LavaTube:
     def __init__(self, npa):
         self.array = npa
-        self.array_basin = np.full_like(self.array, False)
-        self.lows = self.find_lows()
 
     def find_lows(self):
+        """Find low points - the locations that are lower than any of its adjacent locations
+
+        Returns:
+            List: A list of heights at low points
+        """
         low_values = []
         for (row, column), value in np.ndenumerate(self.array):
             row_above, row_below = row - 1, row + 1
@@ -38,6 +41,14 @@ class LavaTube:
         return low_values
 
     def product_largest_basins(self, number_of_basins=None):
+        """The product of the largest basins
+
+        Args:
+            number_of_basins (int, optional): The number of largest basins to include. Defaults to None which includes all basins.
+
+        Returns:
+            int: The product of the "number_of_basins" largest basins
+        """
         basin_sizes = self.find_size_of_basins()
 
         if number_of_basins is not None:
@@ -46,22 +57,37 @@ class LavaTube:
         return np.prod(basin_sizes)
 
     def find_size_of_basins(self):
+        """Find the size of basins, where a basin consists of a group of tube heights that are less than 9
+
+        Returns:
+            List: A list of basin sizes
+        """
         label, _ = ndimage.label(self.array < 9)
         basin_sizes = np.bincount(label.ravel())
         return list(basin_sizes)[1:]
 
     def risk_level(self, height):
+        """Compute the risk level (1 plus the height)
+
+        Args:
+            height (int): The height of the lava tube
+
+        Returns:
+            int: The risk level
+        """
         return 1 + height
 
     def risk_level_sums(self):
+        """Computer the sum of all risk levels
+
+        Returns:
+            int: The sum of all risk levels
+        """
         risk = 0
-        for height in self.lows:
+        for height in self.find_lows():
             risk += self.risk_level(height)
 
         return risk
-
-    def found_low(self, row, column):
-        return self.array_basin[row][column]
 
 
 def load_from_file(filename):
