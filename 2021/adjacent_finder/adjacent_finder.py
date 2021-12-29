0db47983313f299ba55b517dc448f3c1dd52f605
Dan Ottey <dottey@boomi.com>
2021-12-29 07:34:26 -0500

    Create the adjacent finder function.

--- 2021/adjacent_finder/adjacent_finder.py
+++ 2021/adjacent_finder/adjacent_finder.py
@@ -2,6 +2,16 @@ import numpy as np
 
 
 def adjacent_finder(npa, position, steps=1):
+    """Find adjacent coordinates (including diagonals) in a 2d numpy array
+
+    Args:
+        npa (numpy array): An array to search through
+        position (tuple): The (X, Y) coordinate to find adjacent neighbors of
+        steps (int, optional): Max distance away from position to look. Defaults to 1.
+
+    Returns:
+        list: An unsorted list of tuples
+    """
     # Modified from https://stackoverflow.com/questions/51657128/how-to-access-the-adjacent-cells-of-each-elements-of-matrix-in-python
     adjacent = []
 
