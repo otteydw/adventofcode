15db61dd2d1b5803e950bef1c80d36c882a307cc
Dan Ottey <dottey@boomi.com>
2021-12-29 11:24:21 -0500

    2021 Day 11 Part 2 Complete - with cleanup.

--- 2021/day11/day11.py
+++ 2021/day11/day11.py
@@ -30,32 +30,9 @@ def load_into_array(filename):
 
 
 def flash(npa, coordinate):
-    # print(f"Flashing around coordinate {coordinate}")
 
     for adjacent in adjacent_finder.adjacent_finder(npa, coordinate):
         npa[adjacent] += 1
-
-
-# def one_stepb(npa):
-
-#     npa += 1
-#     npa = np.where(npa >= 9, npa+1, npa)
-#     return npa
-
-# def one_stepc(npa):
-#     # Add 1 to all
-#     npa += 1
-
-#     flashed_this_step = np.where(npa >= 0, False, True)
-#     print(flashed_this_step)
-#     # Flash all >=9
-#     coordinates_greater_than_nine = np.where(npa >= 9, True, False)
-#     print(coordinates_greater_than_nine)
-#     # new_npa = np.where(coordinates_greater_than_nine == True and flashed_this_step == False, npa+1, npa)
-#     new_npa = np.all(coordinates_greater_than_nine == True and flashed_this_step == False, npa+1, npa)
-#     print(new_npa)
-
-#     # print(type(new_npa))
 
 
 def one_step(npa):
@@ -68,20 +45,12 @@ def one_step(npa):
 
     # Flash all >9
     coordinates_greater_than_nine_array = np.where(npa > 9)
-    coordinates_greater_than_nine_set = set(zip(coordinates_greater_than_nine_array[0], coordinates_greater_than_nine_array[1]))
-
-    # for coordinate_greater_than_nine in coordinates_greater_than_nine_set:
-    #     if coordinate_greater_than_nine not in flashed_this_step:
-    #             flashed_this_step.add(coordinate_greater_than_nine)
-    #             flash(npa, coordinate_greater_than_nine)
-
-    # coordinates_to_flash = coordinates_greater_than_nine_set - flashed_this_step
-    # # print(f"Flashing coordinates: {coordinates_to_flash}")
-    # for coordinate_to_flash in coordinates_to_flash:
-    #     flash(npa, coordinate_to_flash)
-    #     flashed_this_step.add(coordinate_to_flash)
-    #     flash_count += 1
-
+    coordinates_greater_than_nine_set = set(
+        zip(
+            coordinates_greater_than_nine_array[0],
+            coordinates_greater_than_nine_array[1],
+        )
+    )
     coordinates_to_flash = coordinates_greater_than_nine_set - flashed_this_step
 
     while len(coordinates_to_flash) > 0:
@@ -90,24 +59,29 @@ def one_step(npa):
             flashed_this_step.add(coordinate_to_flash)
             flash_count += 1
         coordinates_greater_than_nine_array = np.where(npa > 9)
-        coordinates_greater_than_nine_set = set(zip(coordinates_greater_than_nine_array[0], coordinates_greater_than_nine_array[1]))
+        coordinates_greater_than_nine_set = set(
+            zip(
+                coordinates_greater_than_nine_array[0],
+                coordinates_greater_than_nine_array[1],
+            )
+        )
         coordinates_to_flash = coordinates_greater_than_nine_set - flashed_this_step
 
     # Reset to 0
-    np.place(npa, npa>9, 0)
+    np.place(npa, npa > 9, 0)
 
     return flash_count
+
 
 def step(npa, steps):
 
     flash_count = 0
 
-    for step in range(steps):
+    for _ in range(steps):
         flash_count += one_step(npa)
-        # print(f"After step {step+1}")
-        # print(npa)
 
     return flash_count
+
 
 def check_simultaneous_flash(npa):
     # Simultaneous flash is where all values are 0 after flashing
@@ -124,50 +98,6 @@ def get_simultaneous_flash_step(npa):
 
     return step_count
 
-# def one_step(npa):
-
-#     # Add 1
-#     npa += 1
-
-#     flashed_this_step = set()
-
-#     # Flash all >=9
-#     coordinates_greater_than_nine = np.where(npa >= 9)
-#     print(coordinates_greater_than_nine)
-
-#     while len(coordinates_greater_than_nine) > 0:
-#         for coordinate_greater_than_nine in coordinates_greater_than_nine:
-#             if coordinate_greater_than_nine not in flashed_this_step:
-#                 flashed_this_step.add(coordinate_greater_than_nine)
-#                 flash(npa, coordinate_greater_than_nine)
-#         coordinates_greater_than_nine = np.where(npa >= 9)
-
-
-
-#     print(npa)
-#     count_of_nines = np.sum(npa == 9)
-#     print(f"Count of nines: {count_of_nines}")
-#     while count_of_nines > 0:
-#         print("Flashing onces")
-#         # Find all the coordinates of 9s
-#         coordinates_of_nines = np.where(npa == 9)
-#         # print("coordinates of nines")
-#         # print(coordinates_of_nines)
-#         # Find the coordinates of all neighbors of nines
-#         coordinates_to_flash = set(coordinates_of_nines)
-#         for coordinate in coordinates_of_nines:
-#             coordinates_to_flash += adjacent_finder(npa, coordinate)
-
-#         # Do the flash around every 9
-#         for coordinate_to_flash in coordinates_to_flash:
-#             flash(npa, coordinate_to_flash)
-
-#         # Recount how many nines there are now
-#         count_of_nines = np.sum(npa == 9)
-
-#     # Reset to 0
-#     np.place(npa, npa>=9, 0)
-#     # print(np.sum(npa == 9))
 
 if __name__ == "__main__":
 
--- 2021/day11/test_day11.py
+++ 2021/day11/test_day11.py
@@ -7,14 +7,20 @@ import day11
 class TestSanta(unittest.TestCase):
     def test_one_step(self):
         octopus_array = day11.load_into_array("example_small/example.txt")
-        print(octopus_array)
-        octopus_array_after_one_step = day11.load_into_array("example_small/after_step_01.txt")
-        octopus_array_after_two_steps = day11.load_into_array("example_small/after_step_02.txt")
+        octopus_array_after_one_step = day11.load_into_array(
+            "example_small/after_step_01.txt"
+        )
+        octopus_array_after_two_steps = day11.load_into_array(
+            "example_small/after_step_02.txt"
+        )
         day11.one_step(octopus_array)
-        print(octopus_array)
-        self.assertEqual(np.array_equal(octopus_array, octopus_array_after_one_step), True)
+        self.assertEqual(
+            np.array_equal(octopus_array, octopus_array_after_one_step), True
+        )
         day11.one_step(octopus_array)
-        self.assertEqual(np.array_equal(octopus_array, octopus_array_after_two_steps), True)
+        self.assertEqual(
+            np.array_equal(octopus_array, octopus_array_after_two_steps), True
+        )
 
     def test_one_step_count(self):
         octopus_array = day11.load_into_array("example_small/example.txt")
@@ -23,9 +29,13 @@ class TestSanta(unittest.TestCase):
 
     def test_step(self):
         octopus_array = day11.load_into_array("example_small/example.txt")
-        octopus_array_after_two_steps = day11.load_into_array("example_small/after_step_02.txt")
+        octopus_array_after_two_steps = day11.load_into_array(
+            "example_small/after_step_02.txt"
+        )
         day11.step(octopus_array, 2)
-        self.assertEqual(np.array_equal(octopus_array, octopus_array_after_two_steps), True)
+        self.assertEqual(
+            np.array_equal(octopus_array, octopus_array_after_two_steps), True
+        )
 
     def test_step_count(self):
         octopus_array = day11.load_into_array("example_small/example.txt")
@@ -36,7 +46,6 @@ class TestSanta(unittest.TestCase):
             octopus_array = day11.load_into_array("example_large/example.txt")
             step_filename = f"example_large/after_step_{str(step_count).zfill(2)}.txt"
             expected_array = day11.load_into_array(step_filename)
-            print(f"Testing {step_count}")
             day11.step(octopus_array, step_count)
             self.assertEqual(np.array_equal(octopus_array, expected_array), True)
 
@@ -60,5 +69,7 @@ class TestSanta(unittest.TestCase):
     def test_get_simultaneous_flash_step(self):
         octopus_array = day11.load_into_array("example_large/example.txt")
         self.assertEqual(day11.get_simultaneous_flash_step(octopus_array), 195)
+
+
 if __name__ == "__main__":
     unittest.main()
