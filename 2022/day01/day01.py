bd281b316df0468797fe9bc3180a1a8a721f8bde
Dan Ottey <dan@bluefrogs.us>
2022-12-02 07:19:21 -0500

    2022 day01 complete (#4)
    * Add pycache to gitignore.
    
    * Update .gitignore
    
    * 2022 Day 01 Part 1 Complete.
    
    * 2022 Day 01 Part 2 Complete.

---
+++ 2022/day01/day01.py
@@ -0,0 +1,47 @@
+import os
+
+
+def load_from_file(filename):
+    input_path = os.path.join(os.path.dirname(__file__), filename)
+
+    data = []
+
+    with open(input_path) as input_file:
+        for line in input_file:
+            data.append(line.rstrip())
+
+    return data
+
+
+def elves_calories(list_of_calories):
+    elves = []
+    current_calorie_total = 0
+    for calorie in list_of_calories:
+        if calorie != "":
+            current_calorie_total += int(calorie)
+        else:
+            elves.append(current_calorie_total)
+            current_calorie_total = 0
+
+    elves.append(current_calorie_total)
+    return elves
+
+
+def most_calorie_elf(list_of_elves):
+    return max(list_of_elves)
+
+
+def total_calories_of_top_elves(list_of_elves, number_of_elves):
+    list_of_elves.sort(reverse=True)
+    return sum(list_of_elves[:number_of_elves])
+
+
+if __name__ == "__main__":
+
+    input_filename = "input.txt"
+    calorie_list = load_from_file(input_filename)
+    these_calories = elves_calories(calorie_list)
+    print(f"Most calories in a single elf: {most_calorie_elf(these_calories)}")
+    print(
+        f"Most calories of top 3 elves: {total_calories_of_top_elves(these_calories, 3)}"
+    )
