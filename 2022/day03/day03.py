4f443b445031528369bbdb515bccc208b239d1a1
Daniel W. Ottey <dottey@boomi.com>
2022-12-03 21:15:40 -0500

    2022 Day 03 Part 2 Complete.

--- 2022/day03/day03.py
+++ 2022/day03/day03.py
@@ -15,25 +15,46 @@ def load_from_file(filename):
 
 class Rucksack:
     def __init__(self, contents):
-        self.rucksack_size = len(contents)
-        self.compartment1 = contents[: (self.rucksack_size // 2)]
-        self.compartment2 = contents[self.rucksack_size // 2 :]
-
+        self.contents = contents
+        self.rucksack_size = len(self.contents)
+        self.compartment1 = self.contents[: (self.rucksack_size // 2)]
+        self.compartment2 = self.contents[self.rucksack_size // 2 :]
         self.common_item = self.get_common_item()
-        self.priority = self.get_priority()
+        self.priority = get_priority(self.common_item)
 
     def get_common_item(self):
         for item in self.compartment1:
             if item in self.compartment2:
                 return item
 
-    def get_priority(self):
-        if self.common_item == self.common_item.lower():
-            difference = 96
-        else:
-            difference = 38
-        priority = ord(self.common_item) - difference
-        return priority
+
+class ElfGroup:
+    def __init__(self, group_of_rucksacks):
+        self.group_of_rucksacks = group_of_rucksacks
+        self.common_item = self.get_common_item()
+        self.priority = get_priority(self.common_item)
+
+    def get_common_item(self):
+        for item in self.group_of_rucksacks[0].contents:
+            if (
+                item in self.group_of_rucksacks[1].contents
+                and item in self.group_of_rucksacks[2].contents
+            ):
+                return item
+
+
+def get_priority(item):
+    if item == item.lower():
+        difference = 96
+    else:
+        difference = 38
+    priority = ord(item) - difference
+    return priority
+
+
+# https://stackoverflow.com/questions/434287/how-to-iterate-over-a-list-in-chunks
+def chunker(seq, size):
+    return (seq[pos : pos + size] for pos in range(0, len(seq), size))
 
 
 def sum_rucksack_priorities(rucksacks):
@@ -45,10 +66,27 @@ def sum_rucksack_priorities(rucksacks):
     return priority_sum
 
 
+def sum_group_priorities(rucksacks):
+    priority_sum = 0
+    for chunk in chunker(rucksacks, 3):
+        group = []
+        for rucksack in chunk:
+            this_sack = Rucksack(rucksack)
+            group.append(this_sack)
+        this_group = ElfGroup(group)
+        priority_sum += this_group.priority
+
+    return priority_sum
+
+
 if __name__ == "__main__":
 
     input_filename = "input.txt"
 
     all_rucksacks = load_from_file(input_filename)
+
     rucksack_priority_sum = sum_rucksack_priorities(all_rucksacks)
     print(f"Sum of priority of those items is: {rucksack_priority_sum}")
+
+    group_priority_sum = sum_group_priorities(all_rucksacks)
+    print(f"Sum of priority of the group items is: {group_priority_sum}")
--- 2022/day03/test_day03.py
+++ 2022/day03/test_day03.py
@@ -1,13 +1,25 @@
 import unittest
 
-from day03 import Rucksack, load_from_file, sum_rucksack_priorities
+from day03 import (
+    Rucksack,
+    ElfGroup,
+    load_from_file,
+    sum_rucksack_priorities,
+    get_priority,
+    sum_group_priorities,
+)
 
 
 class TestSanta(unittest.TestCase):
-    def setUp(self):
-        pass
+    def test_get_priority(self):
+        self.assertEqual(get_priority("p"), 16)
+        self.assertEqual(get_priority("L"), 38)
+        self.assertEqual(get_priority("P"), 42)
+        self.assertEqual(get_priority("v"), 22)
+        self.assertEqual(get_priority("t"), 20)
+        self.assertEqual(get_priority("s"), 19)
 
-    def test_get_common_item(self):
+    def test_rucksack_get_common_item(self):
         mySack = Rucksack("vJrwpWtwJgWrhcsFMMfFFhFp")
         self.assertEqual(mySack.common_item, "p")
         mySack = Rucksack("jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL")
@@ -21,24 +33,28 @@ class TestSanta(unittest.TestCase):
         mySack = Rucksack("CrZsJsPPZsGzwwsLwLmpwMDw")
         self.assertEqual(mySack.common_item, "s")
 
-    def test_get_priority(self):
-        mySack = Rucksack("vJrwpWtwJgWrhcsFMMfFFhFp")
-        self.assertEqual(mySack.priority, 16)
-        mySack = Rucksack("jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL")
-        self.assertEqual(mySack.priority, 38)
-        mySack = Rucksack("PmmdzqPrVvPwwTWBwg")
-        self.assertEqual(mySack.priority, 42)
-        mySack = Rucksack("wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn")
-        self.assertEqual(mySack.priority, 22)
-        mySack = Rucksack("ttgJtRGJQctTZtZT")
-        self.assertEqual(mySack.priority, 20)
-        mySack = Rucksack("CrZsJsPPZsGzwwsLwLmpwMDw")
-        self.assertEqual(mySack.priority, 19)
-
     def test_sum_rucksack_priorities(self):
         input_filename = "example.txt"
         all_rucksacks = load_from_file(input_filename)
         self.assertEqual(sum_rucksack_priorities(all_rucksacks), 157)
+
+    def test_group_get_common_item(self):
+        sack1 = Rucksack("vJrwpWtwJgWrhcsFMMfFFhFp")
+        sack2 = Rucksack("jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL")
+        sack3 = Rucksack("PmmdzqPrVvPwwTWBwg")
+        myGroup = ElfGroup([sack1, sack2, sack3])
+        self.assertEqual(myGroup.common_item, "r")
+
+        sack1 = Rucksack("wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn")
+        sack2 = Rucksack("ttgJtRGJQctTZtZT")
+        sack3 = Rucksack("CrZsJsPPZsGzwwsLwLmpwMDw")
+        myGroup = ElfGroup([sack1, sack2, sack3])
+        self.assertEqual(myGroup.common_item, "Z")
+
+    def test_sum_group_priorities(self):
+        input_filename = "example.txt"
+        all_rucksacks = load_from_file(input_filename)
+        self.assertEqual(sum_group_priorities(all_rucksacks), 70)
 
 
 if __name__ == "__main__":
