079f89803325c11504d5962f779c9e2fdd654b4d
Daniel W. Ottey <dottey@boomi.com>
2022-12-11 21:15:19 -0500

    2022 Day 7 Part 1 Complete

--- 2022/day07/day07.py
+++ 2022/day07/day07.py
@@ -12,116 +12,106 @@ def load_from_file(filename):
 
     return data
 
+
 class Node:
-    def __init__(self, name, parent):
+    def __init__(self, name, type, parent, size=0):
         self.name = name
         self.parent = parent
-
-class Directory(Node):
-    def __init__(self, name, parent):
-        super().__init__(name, parent)
         self.children = []
-        # self.size = 0
+        self.size = 0
+        self.type = type
 
-    def add(self, child):
+        if self.isFile():
+            self.increase_size(size)
+
+    def increase_size(self, size):
+        self.size += size
+        if self.parent != None:
+            self.parent.increase_size(size)
+
+    def get_size(self):
+        return self.size
+
+    def get_type(self):
+        return self.type
+
+    def isDirectory(self):
+        return self.type == "directory"
+
+    def isFile(self):
+        return self.type == "file"
+
+    def add_child(self, child):
         self.children.append(child)
 
-class File(Node):
-    def __init__(self, name, parent, size=0):
-        super().__init__(name, parent)
-        self.size=size
-        # self.parent.size += self.size
+    def print_tree(self, indent=0):
+        if self.isDirectory():
+            print(" " * indent + "- " + self.name + " (dir) " + str(self.size))
+            for child in self.children:
+                child.print_tree(indent + 2)
+        else:
+            print(
+                " " * indent + "- " + self.name + " (file, size=" + str(self.size) + ")"
+            )
 
-def print_tree(root, indent=0):
-    # print(' '*indent + '- ' + root.name)
-    if hasattr(root, 'children'):
-        print(' '*indent + '- ' + root.name + ' (dir)')
-        for child in root.children:
-            print_tree(child, indent+2)
-    else:
-        print(' '*indent + '- ' + root.name + ' (file, size=' + str(root.size) + ')')
+    def get_directories(self):
+        list_of_directories = []
+
+        if self.isDirectory():
+            list_of_directories.append(self)
+            for child in self.children:
+                list_of_directories += child.get_directories()
+        return list_of_directories
+
+    def sum_dirs_under_size(self, max_size):
+        list_of_dirs = self.get_directories()
+
+        sum_size = 0
+
+        for dir in list_of_dirs:
+            if dir.size <= max_size:
+                sum_size += dir.size
+
+        return sum_size
+
 
 def parse_terminal_output(terminal_output, current_dir=None):
 
     if current_dir == None:
-        # current_directory = None
-        root = Directory('/', None)
+        root = Node("/", "directory", None)
         current_directory = root
     else:
-        current_directory=current_dir
+        current_directory = current_dir
 
     for line in terminal_output:
-        if line.startswith('$ cd'):
-            dir_name = line.split(' ')[2]
-            if dir_name == '..':
+        if line.startswith("$ cd"):
+            dir_name = line.split(" ")[2]
+            if dir_name == "..":
                 current_directory = current_directory.parent
             for child in current_directory.children:
                 if child.name == dir_name:
                     current_directory = child
-        elif line.startswith('$ ls'):
+        elif line.startswith("$ ls"):
             pass
-        elif line.startswith('dir '):
-            new_dir_name = line.split(' ')[1]
-            # print('new dir ' + new_dir_name)
-            new_dir = Directory(new_dir_name, current_directory)
-            current_directory.add(new_dir)
+        elif line.startswith("dir "):
+            new_dir_name = line.split(" ")[1]
+            new_dir = Node(new_dir_name, "directory", current_directory)
+            current_directory.add_child(new_dir)
         else:
-            file_size, file_name = line.split(' ')
-            # print(file_size, file_name)
-            new_file = File(file_name, current_directory, int(file_size))
-            current_directory.add(new_file)
-        # print(f'Currently in dir {current_directory.name}')
+            file_size, file_name = line.split(" ")
+            new_file = Node(file_name, "file", current_directory, int(file_size))
+            current_directory.add_child(new_file)
 
     return root
 
-def find_dirs_above_size(root, min_size):
-    total_size = 0
-
-    if hasattr(root, 'children'):
-        for child in root.children:
-            total_size += find_dirs_above_size(child, min_size)
-    else:
-        total_size += root.size
-
-    # if total_size > min_size:
-    #     total_size = 0
-    return total_size
-
-def find_dirs_above_size2(root, min_size):
-    # total_size = 0
-
-    if hasattr(root, 'children'):
-        children_size = 0
-        for child in root.children:
-            children_size += find_dirs_above_size2(child, min_size)
-
-    else:
-        total_size += root.size
-
-    # if total_size > min_size:
-    #     total_size = 0
-    return total_size
 
 if __name__ == "__main__":
 
-    input_filename = "example.txt"
+    # input_filename = "example.txt"
+    input_filename = "input.txt"
 
     data = load_from_file(input_filename)
 
-    # root = Directory('/', None)
-    # dir = Directory('a', root)
-
-    # root.add(dir)
-
-    # file = File(name='a.txt', parent=dir)
-    # dir.add(file)
-    # file = File(name='b.txt', parent=dir)
-    # dir.add(file)
-
-    # print_tree(root)
-
     filesystem = parse_terminal_output(data)
-
-    print_tree(filesystem)
-
-    print(find_dirs_above_size(filesystem, 100000))
+    filesystem.print_tree()
+    print(filesystem.sum_dirs_under_size(100000))
