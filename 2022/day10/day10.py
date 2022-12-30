ba879ac57b2eefdf58614656d7df482285009ebc
Dan Ottey <dan@bluefrogs.us>
2022-12-30 18:21:47 -0500

    2022 day10 (#13)
    * 2022 Day 10 Part 1 Complete.
    * 2022 Day 10 Part 2 Complete.

---
+++ 2022/day10/day10.py
@@ -0,0 +1,81 @@
+import os
+
+
+def load_from_file(filename):
+    input_path = os.path.join(os.path.dirname(__file__), filename)
+
+    data = []
+
+    with open(input_path) as input_file:
+        for line in input_file:
+            data.append(line.rstrip())
+
+    return data
+
+
+class Tube:
+    def __init__(self, filename):
+        self.program = load_from_file(filename)
+        self.X = 1
+        self.X_history = {}
+        self.cycles = 0
+        self.update_register()
+        self.process_program()
+
+    def process_program(self):
+        for instruction in self.program:
+            if instruction == "noop":
+                self.cycles += 1
+                self.update_register()
+            elif instruction.startswith("addx"):
+                ticks = 2
+                for _ in range(ticks):
+                    self.cycles += 1
+                    self.update_register()
+                self.X += int(instruction.split(" ")[1])
+                self.update_register()
+
+    def update_register(self):
+        self.X_history[self.cycles] = self.X
+
+    def signal_strength(self, cycle):
+        # "During" means the value at the end of the previous cycle
+        return cycle * self.X_history[cycle - 1]
+
+    def sum_of_signal_strengths(self, signals):
+        sum = 0
+        for signal in signals:
+            sum += self.signal_strength(signal)
+        return sum
+
+    def draw(self):
+        width = 40
+        height = 6
+        screen = [["."] * width for i in range(height)]
+
+        for cycle in range(240):
+            row = cycle // width
+            column = cycle % width
+            sprite_middle = self.X_history[cycle]
+
+            if column in [sprite_middle - 1, sprite_middle, sprite_middle + 1]:
+                screen[row][column] = "#"
+
+        for row in screen:
+            for column in row:
+                print(column, end="")
+            print()
+
+
+if __name__ == "__main__":
+
+    # input_filename = "small_program.txt"
+    # input_filename = "larger_program.txt"
+    input_filename = "input.txt"
+
+    tube = Tube(input_filename)
+
+    interesting_signals = [20, 60, 100, 140, 180, 220]
+    print(f"Sum of signals: {tube.sum_of_signal_strengths(interesting_signals)}")
+    print()
+    tube.draw()
---
+++ 2022/day10/test_day10.py
@@ -0,0 +1,30 @@
+import unittest
+
+from day10 import Tube
+
+
+class TestSanta(unittest.TestCase):
+    def test_X_history(self):
+        input_filename = "small_program.txt"
+        self.tube = Tube(input_filename)
+        self.assertEqual(self.tube.X_history[5], -1)
+
+    def test_signal_strength(self):
+        input_filename = "larger_program.txt"
+        self.tube = Tube(input_filename)
+        self.assertEqual(self.tube.signal_strength(20), 420)
+        self.assertEqual(self.tube.signal_strength(60), 1140)
+        self.assertEqual(self.tube.signal_strength(100), 1800)
+        self.assertEqual(self.tube.signal_strength(140), 2940)
+        self.assertEqual(self.tube.signal_strength(180), 2880)
+        self.assertEqual(self.tube.signal_strength(220), 3960)
+
+    def test_sum_of_signal_strengths(self):
+        input_filename = "larger_program.txt"
+        self.tube = Tube(input_filename)
+        interesting_signals = [20, 60, 100, 140, 180, 220]
+        self.assertEqual(self.tube.sum_of_signal_strengths(interesting_signals), 13140)
+
+
+if __name__ == "__main__":
+    unittest.main()
