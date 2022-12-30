c8944f53846896516849bb97681ab34bb92558b6
Daniel W. Ottey <dottey@boomi.com>
2022-12-30 16:15:04 -0500

    Forgot to remove the commented code during cleanup.

--- 2022/day09/test_day09.py
+++ 2022/day09/test_day09.py
@@ -4,9 +4,6 @@ from day09 import Rope
 
 
 class TestSanta(unittest.TestCase):
-    # def setUp(self):
-    #     input_filename = "example.txt"
-    #     self.rope = Rope(input_filename)
 
     def test_count_tail_visited_positions(self):
         input_filename = "example.txt"
@@ -20,7 +17,6 @@ class TestSanta(unittest.TestCase):
         input_filename = "example2.txt"
         self.rope = Rope(input_filename, knots=10)
         self.assertEqual(self.rope.count_tail_visited_positions(), 36)
-
 
 if __name__ == "__main__":
     unittest.main()
