f3d2e1a02b9301e8f809fa4ed44834c44972ee09
Daniel W. Ottey <dottey@boomi.com>
2022-12-30 17:15:58 -0500

    2022 Day 10 Part 1 - Test failed but solution was correct...

---
+++ 2022/day10/day10.py
@@ -0,0 +1,67 @@
+import os
+import pprint
+
+
+def load_from_file(filename):
+    input_path = os.path.join(os.path.dirname(__file__), filename)
+
+    data = []
+
+    with open(input_path) as input_file:
+        for line in input_file:
+            data.append(line.rstrip())
+
+    return data
+
+class Tube:
+    def __init__(self, filename):
+        self.program = load_from_file(filename)
+        self.X = 1
+        self.X_history = {}
+        self.cycles = 0
+        # self.status_report()
+        self.process_program()
+        # self.status_report()
+
+    def process_program(self):
+        for instruction in self.program:
+            if instruction == "noop":
+                self.cycles += 1
+                self.update_register()
+            elif instruction.startswith("addx"):
+                ticks = 2
+                for _ in range(ticks):
+                    self.cycles += 1
+                    self.update_register()
+                self.X += int(instruction.split(" ")[1])
+                self.update_register()
+
+    def update_register(self):
+        self.X_history[self.cycles] = self.X
+
+    def status_report(self):
+        pprint.pprint(self.X_history)
+
+    def signal_strength(self, cycle):
+        return cycle * self.X_history[cycle]
+
+    def sum_of_signal_strengths(self, signals):
+        sum = 0
+        for signal in signals:
+            sum += self.signal_strength(signal)
+        return sum
+
+if __name__ == "__main__":
+
+    # input_filename = "small_program.txt"
+    input_filename = "input.txt"
+
+    tube = Tube(input_filename)
+    # for knots in [2, 10]:
+    #     rope = Rope(input_filename, knots=knots)
+    #     print(
+    #         f"Rope with {knots} knots: Tail visited {rope.count_tail_visited_positions()} positions."
+    #     )
+    # tube.status_report()
+    interesting_signals=[20, 60, 100, 140, 180, 220]
+    print(f"Sum of signals: {tube.sum_of_signal_strengths(interesting_signals)}")
