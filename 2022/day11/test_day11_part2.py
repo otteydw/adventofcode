78fb5e261376ef70a2a4b3a344b21cb35e8d63cc
Dan Ottey <dan@bluefrogs.us>
2023-01-02 08:33:05 -0500

    2022 day11 (#14)
    * 2022 Day 11 Part 1 Complete.
    * 2022 Day 11 Part 2... Probably correct but super slow.
    * 2022 Day 11 Part 2 Complete.

---
+++ 2022/day11/test_day11_part2.py
@@ -0,0 +1,23 @@
+import unittest
+
+from day11_part2 import Game
+
+
+class TestSanta(unittest.TestCase):
+    def test_monkey_business(self):
+        input_filename = "example.txt"
+        game = Game(input_filename, rounds=20)
+        self.assertEqual(game.monkey_business(), 10605)
+
+        game = Game(input_filename, rounds=10000, crt=True)
+        self.assertEqual(game.monkey_business(), 2713310158)
+
+        # game = Game(input_filename, rounds=1000, relief_enabled=False)
+        # self.assertEqual(game.monkey_business(), 27019168)
+
+        # game = Game(input_filename, rounds=10000, relief_enabled=False)
+        # self.assertEqual(game.monkey_business(), 2713310158)
+
+
+if __name__ == "__main__":
+    unittest.main()
