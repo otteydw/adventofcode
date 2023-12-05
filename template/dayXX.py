822724da4c0ba7e936ed41bfbb7bc01b98a9e46c
Daniel W. Ottey <dan.ottey@boomi.com>
2023-12-05 07:47:47 -0500

    Add template from realpython

---
+++ template/dayXX.py
@@ -0,0 +1,32 @@
+import pathlib
+import sys
+
+
+def parse(puzzle_input):
+    return [line for line in puzzle_input.split("\n")]
+
+
+def part1(data):
+    pass
+
+
+def part2(data):
+    pass
+
+
+def solve(puzzle_input):
+    """Solve the puzzle for the given input."""
+    data = parse(puzzle_input)
+    solution1 = part1(data)
+    solution2 = part2(data)
+
+    return solution1, solution2
+
+
+if __name__ == "__main__":
+    for path in sys.argv[1:]:
+        print(f"{path}:")
+        puzzle_input = pathlib.Path(path).read_text().strip()
+        solutions = solve(puzzle_input)
+        for solution_number, solution in enumerate(solutions):
+            print(f"Solution {solution_number}: {str(solution)}")
