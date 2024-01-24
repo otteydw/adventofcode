95f0eebe102252c564c68f6e7ec799d6aac22262
Daniel W. Ottey <dan.ottey@boomi.com>
2024-01-23 21:53:38 -0500

    2023 Day 14 Parts 1 and 2 Complete

--- 2023/day14/day14.py
+++ 2023/day14/day14.py
@@ -1,56 +1,46 @@
+import copy
 import pathlib
 import sys
-import itertools
-import copy
 
 import numpy as np
-from tqdm import tqdm
 
-def split_list(lst, val):
-    return [
-        list(group) for k, group in itertools.groupby(lst, lambda x: x == val) if not k
-    ]
 
 def parse(puzzle_input):
     # return [line for line in puzzle_input.split("\n")]
     lines = [line for line in puzzle_input.split("\n")]
     return lines
-    # return split_list(lines, "")
+
 
 class Dish:
     def __init__(self, grid):
-        # print(grid)
-
         self.grid = []
         for row in grid:
             self.grid.append([character for character in row])
         self.grid = np.array(self.grid)
 
     def __repr__(self):
-        # return str(self.grid)
-        output=''
+        output = ""
         for row in self.grid:
             for character in row:
                 output += character
-            output+='\n'
+            output += "\n"
         return output
 
     def __eq__(self, other):
         return np.array_equiv(self.grid, other.grid)
-        # return (self.grid == other.grid).all()
 
     def tilt_column(self, column_number):
         # Always tilts "north"
-
-        # print(f'Tilt column {column_number}')
         column_height = len(self.grid)
 
         number_of_moves = 0
         for row in range(1, column_height):
-            # print(f'Tilting row {row} column {column_number}')
-            if self.grid[row - 1][column_number] == '.' and self.grid[row][column_number] == 'O':
-                self.grid[row - 1][column_number] = 'O'
-                self.grid[row][column_number] = '.'
+            if (
+                self.grid[row - 1][column_number] == "."
+                and self.grid[row][column_number] == "O"
+            ):
+                self.grid[row - 1][column_number] = "O"
+                self.grid[row][column_number] = "."
                 number_of_moves += 1
 
         if number_of_moves != 0:
@@ -63,62 +53,45 @@ class Dish:
 
     def cycle(self):
         for _ in range(4):
-            self.tilt() # Tilt North
-            self.grid = np.rot90(self.grid, 3) # Rotate 90 degrees to the "right".  West is now the new North
+            self.tilt()  # Tilt North
+            self.grid = np.rot90(
+                self.grid, 3
+            )  # Rotate 90 degrees to the "right".  West is now the new North
 
     def calculate_load(self):
-        # print(self.grid)
-        # print()
-        flipped = np.flipud(self.grid)
-        # print(flipped)
+        flipped = np.flipud(self.grid)  # Flipped makes the counting easier.
         load = 0
         for row_number, row in enumerate(flipped):
-            # print(list(row))
-            row_power = (row_number+1) * sum(1 for i in row if i == 'O')
-            # print(row_power)
+            row_power = (row_number + 1) * sum(1 for i in row if i == "O")
             load += row_power
         return load
 
+
 def part1(dish):
-    # for dish in data:
-        # print(dish)
-        # print()
     this_dish = Dish(dish)
-    # print(this_dish)
     this_dish.tilt()
-    # print(this_dish)
-    # this_dish.calculate_load()
     return this_dish.calculate_load()
 
 
 def part2(dish):
     this_dish = Dish(dish)
-    total_cycles=1000000000
-    # total_cycles=1000
+    total_cycles = 1000000000
     previous_grids = {}
-    # total_cycles=6
-    # for _ in tqdm(range(total_cycles)):
-    for iteration in range(1, total_cycles+1):
+    for iteration in range(1, total_cycles + 1):
         print(iteration)
-        # print(this_dish)
-        # print(this_dish.calculate_load())
-        # print(f"Cycle {x} / {total_cycles}")
-        # pre_cycle = copy.deepcopy(this_dish)
         this_dish.cycle()
         if this_dish in previous_grids.values():
-        # if this_dish.grid in previous_grids:
             print(f"Repeating at iteration {iteration}.")
-            previous_iteration = [key for key, val in previous_grids.items() if val == this_dish][0]
+            previous_iteration = [
+                key for key, val in previous_grids.items() if val == this_dish
+            ][0]
             print(f"Previous iteration {previous_iteration}")
-
-            matching_cycle = (total_cycles - previous_iteration) % (iteration - previous_iteration) + previous_iteration
+            matching_cycle = (total_cycles - previous_iteration) % (
+                iteration - previous_iteration
+            ) + previous_iteration
             print(f"We want the value from {matching_cycle}")
-            # print(this_dish)
-            # print()
-            # print(previous_grids[previous_iteration])
             break
         previous_grids[iteration] = copy.deepcopy(this_dish)
-        # previous_grids[this_dish.grid] = iteration
 
     return previous_grids[matching_cycle].calculate_load()
 
--- 2023/day14/test_day14.py
+++ 2023/day14/test_day14.py
@@ -14,29 +14,28 @@ def example1():
     puzzle_input = (PUZZLE_DIR / "example1.txt").read_text().strip()
     return aoc.parse(puzzle_input)
 
+
 @pytest.fixture
 def example1_tilted():
     puzzle_input = (PUZZLE_DIR / "example1_tilted.txt").read_text().strip()
     return aoc.parse(puzzle_input)
+
 
 @pytest.fixture
 def example1_cycle1():
     puzzle_input = (PUZZLE_DIR / "example1_cycle1.txt").read_text().strip()
     return aoc.parse(puzzle_input)
 
+
 @pytest.fixture
 def example1_cycle2():
     puzzle_input = (PUZZLE_DIR / "example1_cycle2.txt").read_text().strip()
     return aoc.parse(puzzle_input)
 
+
 @pytest.fixture
 def example1_cycle3():
     puzzle_input = (PUZZLE_DIR / "example1_cycle3.txt").read_text().strip()
-    return aoc.parse(puzzle_input)
-
-@pytest.fixture
-def example2():
-    puzzle_input = (PUZZLE_DIR / "example2.txt").read_text().strip()
     return aoc.parse(puzzle_input)
 
 
@@ -46,34 +45,22 @@ def test_parse_example1(example1):
     assert example1 == ...
 
 
-# @pytest.mark.skip(reason="Not implemented")
 def test_part1_example1(example1):
     """Test part 1 on example input."""
     assert aoc.part1(example1) == 136
 
 
-@pytest.mark.skip(reason="Not implemented")
 def test_part2_example1(example1):
     """Test part 2 on example input."""
     assert aoc.part2(example1) == 64
 
 
-@pytest.mark.skip(reason="Not implemented")
-def test_part2_example2(example2):
-    """Test part 2 on example input."""
-    assert aoc.part2(example2) == ...
-
 def test_tilting(example1, example1_tilted):
     dish1 = aoc.Dish(example1)
     dish1_tilted = aoc.Dish(example1_tilted)
-    # print(dish1)
     dish1.tilt()
-    # print(dish1)
-    # print()
-    # print(dish1_tilted)
-
-    # assert True
     assert dish1 == dish1_tilted
+
 
 def test_cycle(example1, example1_cycle1, example1_cycle2, example1_cycle3):
     dish1 = aoc.Dish(example1)
