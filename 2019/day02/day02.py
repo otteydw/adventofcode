f0c1aef9ec8d0b9a1fa7eb0045571f10b46b0c0d
Daniel W. Ottey <dan.ottey@boomi.com>
2024-11-12 20:59:19 -0500

    Functional tests.

---
+++ 2019/day02/day02.py
@@ -0,0 +1,89 @@
+import argparse
+import pathlib
+from typing import List
+
+
+def parse(puzzle_input):
+    lines = [line for line in puzzle_input.split("\n")]
+    program = [int(value) for value in lines[0].split(",")]
+    return program
+
+
+def get_opcode(program: List, position: int) -> int:
+    return program[position]
+
+
+def opcode_add(program: List, current_position: int) -> None:
+    # Opcode 1 adds together numbers read from two positions and stores the result in a third position.
+    # The three integers immediately after the opcode tell you these three positions -
+    # the first two indicate the positions from which you should read the input values, and the third indicates
+    # the position at which the output should be stored.
+    position1 = program[current_position + 1]
+    position2 = program[current_position + 2]
+    storage_position = program[current_position + 3]
+    value1 = program[position1]
+    value2 = program[position2]
+    program[storage_position] = value1 + value2
+    return None
+
+
+def opcode_multiply(program: List, current_position: int) -> None:
+    # Opcode 2 works exactly like opcode 1, except it multiplies the two inputs instead of adding them. Again, the three
+    # integers after the opcode indicate where the inputs and outputs are, not their values.
+    position1 = program[current_position + 1]
+    position2 = program[current_position + 2]
+    storage_position = program[current_position + 3]
+    value1 = program[position1]
+    value2 = program[position2]
+    program[storage_position] = value1 * value2
+
+
+def run_program(program: List):
+    current_position = 0
+    done = False
+
+    while not done:
+        opcode = get_opcode(program, current_position)
+
+        match opcode:
+            case 1:
+                opcode_add(program, current_position)
+            case 2:
+                opcode_multiply(program, current_position)
+            case 99:
+                done = True
+            case _:
+                pass
+
+        current_position += 4
+    return program
+
+
+def part1(data: List):
+    return run_program(data)
+
+
+def part2(data):
+    pass
+
+
+def solve(puzzle_input):
+    """Solve the puzzle for the given input."""
+    data = parse(puzzle_input)
+    solution1 = part1(data)
+    solution2 = part2(data)
+
+    return solution1, solution2
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Solve Advent of Code puzzles")
+    parser.add_argument("files", nargs="+", help="Input files to process")
+    args = parser.parse_args()
+
+    for path in args.files:
+        print(f"{path}:")
+        puzzle_input = pathlib.Path(path).read_text().strip()
+        solutions = solve(puzzle_input)
+        for solution_number, solution in enumerate(solutions):
+            print(f"Solution {solution_number}: {str(solution)}")
---
+++ 2019/day02/test_day02.py
@@ -0,0 +1,71 @@
+# test_aoc_template.py
+
+import pathlib
+from typing import List
+
+import day02 as aoc
+import pytest
+
+PUZZLE_DIR = pathlib.Path(__file__).parent
+
+
+@pytest.mark.parametrize(
+    ("input_s", "expected"),
+    (
+        ("1,0,0,0,99", [1, 0, 0, 0, 99]),
+        ("2,3,0,3,99", [2, 3, 0, 3, 99]),
+        ("2,4,4,5,99,0", [2, 4, 4, 5, 99, 0]),
+        ("1,1,1,4,99,5,6,0,99", [1, 1, 1, 4, 99, 5, 6, 0, 99]),
+    ),
+)
+def test_parse(input_s: str, expected: List) -> None:
+    assert aoc.parse(input_s) == expected
+
+
+@pytest.mark.parametrize(
+    ("input_l", "expected"),
+    (
+        ([1, 0, 0, 0, 99], [2, 0, 0, 0, 99]),
+        ([2, 3, 0, 3, 99], [2, 3, 0, 6, 99]),
+        ([2, 4, 4, 5, 99, 0], [2, 4, 4, 5, 99, 9801]),
+        ([1, 1, 1, 4, 99, 5, 6, 0, 99], [30, 1, 1, 4, 2, 5, 6, 0, 99]),
+    ),
+)
+def test_part1(input_l: List, expected: List) -> None:
+    assert aoc.part1(input_l) == expected
+
+
+# @pytest.fixture
+# def example1():
+#     puzzle_input = (PUZZLE_DIR / "example1.txt").read_text().strip()
+#     return aoc.parse(puzzle_input)
+
+
+# @pytest.fixture
+# def example2():
+#     puzzle_input = (PUZZLE_DIR / "example2.txt").read_text().strip()
+#     return aoc.parse(puzzle_input)
+
+
+# @pytest.mark.skip(reason="Not implemented")
+# def test_parse_example1(example1):
+#     """Test that input is parsed properly."""
+#     assert example1 == ...
+
+
+# @pytest.mark.skip(reason="Not implemented")
+# def test_part1_example1(example1):
+#     """Test part 1 on example input."""
+#     assert aoc.part1(example1) == ...
+
+
+# @pytest.mark.skip(reason="Not implemented")
+# def test_part2_example1(example1):
+#     """Test part 2 on example input."""
+#     assert aoc.part2(example1) == ...
+
+
+# @pytest.mark.skip(reason="Not implemented")
+# def test_part2_example2(example2):
+#     """Test part 2 on example input."""
+#     assert aoc.part2(example2) == ...
