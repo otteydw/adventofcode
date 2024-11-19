48f2df68be8a8fd36c4596727f0c02549a745980
Daniel W. Ottey <dan.ottey@boomi.com>
2024-11-19 12:31:50 -0500

    Rearrange comments

--- 2019/day05/test_day05.py
+++ 2019/day05/test_day05.py
@@ -30,14 +30,10 @@ def test_parse(input_s: str, expected: List) -> None:
         ([2, 3, 0, 3, 99], [2, 3, 0, 6, 99]),
         ([2, 4, 4, 5, 99, 0], [2, 4, 4, 5, 99, 9801]),
         ([1, 1, 1, 4, 99, 5, 6, 0, 99], [30, 1, 1, 4, 2, 5, 6, 0, 99]),
-        (
-            [101, 4, 0, 0, 99],
-            [105, 4, 0, 0, 99],
-        ),  # literal 4 added to 101 from position 0 = 105 stored in position 0
-        (
-            [102, 4, 0, 0, 99],
-            [408, 4, 0, 0, 99],
-        ),  # literal 4 multiplied by 102 from position 0 = 408 stored in position 0
+        # literal 4 added to 101 from position 0 = 105 stored in position 0
+        ([101, 4, 0, 0, 99], [105, 4, 0, 0, 99]),
+        # literal 4 multiplied by 102 from position 0 = 408 stored in position 0
+        ([102, 4, 0, 0, 99], [408, 4, 0, 0, 99]),
         ([1101, 100, -1, 4, 0], [1101, 100, -1, 4, 99]),
     ),
 )
