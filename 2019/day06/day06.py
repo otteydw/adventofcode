11e0f8ce0e7ebf605e2ed0055e79dace9825103c
Daniel W. Ottey <dan.ottey@boomi.com>
2024-11-20 08:42:57 -0500

    2019 Day 6 Part 2 Complete.

--- 2019/day06/day06.py
+++ 2019/day06/day06.py
@@ -6,20 +6,12 @@ from collections import defaultdict
 def parse(puzzle_input):
     lines = [line for line in puzzle_input.split("\n")]
     print(lines)
-    orbits = defaultdict(list)
+    orbits = defaultdict(lambda: None)
     for line in lines:
-        # print(line)
         objectA, objectB = line.split(")")
-        # orbits[objectA].append(objectB)
-        orbits[objectB].append(objectA)
+        orbits[objectB] = objectA
     print(orbits)
     return orbits
-
-
-# def orbits_directly(orbit_dict: dict, objectA: str, objectB: str):
-#     """ Based on orbit_dict, return True if objectA orbits directly around objectB"""
-#     # return objectA in orbit_dict[objectB]
-#     return objectB in orbit_dict[objectA]
 
 
 def total_orbits_in_map(orbit_dict: dict) -> int:
@@ -37,11 +29,63 @@ def total_orbits_for_object(orbit_dict: dict, obj: str) -> int:
     if obj not in orbit_dict:
         return 0
 
-    orbits = 1
-    for inner_object in orbit_dict[obj]:
-        orbits += total_orbits_for_object(orbit_dict, inner_object)
+    inner_object = orbit_dict[obj]
+    orbits = 1 + total_orbits_for_object(orbit_dict, inner_object)
 
     return orbits
+
+
+def slice_list_up_to_element(lst: list, element):
+    """Given a list, return the slice of the list from beginning up to the first matching element"""
+    index_of_element = lst.index(element)
+    slice = lst[:index_of_element]
+    return slice
+
+
+def minimum_orbital_transfers(orbit_dict: dict, from_object: str, to_object: str):
+    """Given orbit_dict, return the minimum number of orbital transfers required to move from from_object to to_object"""
+    closest_ancestor = find_closest_common_ancestor(orbit_dict, from_object, to_object)
+
+    from_orbit_tree = orbit_tree(orbit_dict, from_object)
+    to_orbit_tree = orbit_tree(orbit_dict, to_object)
+
+    from_orbit_to_common_slice = slice_list_up_to_element(from_orbit_tree, closest_ancestor)
+    to_orbit_to_common_slice = slice_list_up_to_element(to_orbit_tree, closest_ancestor)
+
+    transfers = len(from_orbit_to_common_slice) + len(to_orbit_to_common_slice)
+
+    return transfers
+
+
+def get_object_orbited_by_object(orbit_dict: dict, obj: str) -> str:
+    """Given orbit_dict and the name of an object, return the object this object orbits."""
+    return orbit_dict[obj]
+
+
+def minimum_orbital_transfers_from_you_to_san(orbit_dict: dict):
+    """Given orbit_dict, return the minimum number of orbital transfers required to move from the object YOU are orbiting to the object SAN is orbiting"""
+    object_orbited_by_you = get_object_orbited_by_object(orbit_dict, "YOU")
+    object_orbited_by_san = get_object_orbited_by_object(orbit_dict, "SAN")
+
+    return minimum_orbital_transfers(orbit_dict, object_orbited_by_you, object_orbited_by_san)
+
+
+def orbit_tree(orbit_dict: dict, obj: str):
+    """Given orbit_dict and the name of an object, return the tree of orbits"""
+
+    this_obj_orbits = get_object_orbited_by_object(orbit_dict, obj)
+    if not this_obj_orbits:
+        return [obj]
+
+    return [obj] + orbit_tree(orbit_dict, this_obj_orbits)
+
+
+def find_closest_common_ancestor(orbit_dict: dict, objectA: str, objectB: str):
+    """Given orbit_dict and the name of two objects, find the closest common ancestor that they both orbit"""
+    treeA = orbit_tree(orbit_dict, objectA)
+    treeB = orbit_tree(orbit_dict, objectB)
+
+    return next((ancestor for ancestor in treeA if ancestor in treeB), None)
 
 
 def part1(data):
@@ -49,14 +93,14 @@ def part1(data):
 
 
 def part2(data):
-    pass
+    return minimum_orbital_transfers_from_you_to_san(data)
 
 
 def solve(puzzle_input):
     """Solve the puzzle for the given input."""
     data = parse(puzzle_input)
     solve1 = True
-    solve2 = False
+    solve2 = True
     solution1 = part1(data) if solve1 else None
     solution2 = part2(data) if solve2 else None
 
--- 2019/day06/test_day06.py
+++ 2019/day06/test_day06.py
@@ -14,48 +14,28 @@ def example1():
     return aoc.parse(puzzle_input)
 
 
-# @pytest.fixture
-# def example2():
-#     puzzle_input = (PUZZLE_DIR / "example2.txt").read_text().strip()
-#     return aoc.parse(puzzle_input)
+@pytest.fixture
+def example2():
+    puzzle_input = (PUZZLE_DIR / "example2.txt").read_text().strip()
+    return aoc.parse(puzzle_input)
 
 
 # @pytest.mark.skip(reason="Not implemented")
 def test_parse_example1(example1):
     """Test that input is parsed properly."""
-    # assert example1 == {
-    #     "COM": ["B"],
-    #     "B": ["C", "G"],
-    #     "C": ["D"],
-    #     "D": ["E", "I"],
-    #     "E": ["F", "J"],
-    #     "G": ["H"],
-    #     "J": ["K"],
-    #     "K": ["L"],
-    # }
     assert example1 == {
-        "B": ["COM"],
-        "C": ["B"],
-        "D": ["C"],
-        "E": ["D"],
-        "F": ["E"],
-        "G": ["B"],
-        "H": ["G"],
-        "I": ["D"],
-        "J": ["E"],
-        "K": ["J"],
-        "L": ["K"],
+        "B": "COM",
+        "C": "B",
+        "D": "C",
+        "E": "D",
+        "F": "E",
+        "G": "B",
+        "H": "G",
+        "I": "D",
+        "J": "E",
+        "K": "J",
+        "L": "K",
     }
-
-
-# def test_orbits_directly(example1):
-#     assert aoc.orbits_directly(example1, "D", "C") is True
-#     assert aoc.orbits_directly(example1, "L", "K") is True
-#     assert aoc.orbits_directly(example1, "C", "D") is False
-
-# def test_orbits_indirectly(example1):
-#     assert aoc.orbits_indirectly(example1, "D", "C") is True
-#     assert aoc.orbits_indirectly(example1, "C", "D") is False
 
 
 def test_total_orbits_for_object(example1):
@@ -66,6 +46,31 @@ def test_total_orbits_for_object(example1):
 
 def total_orbits_in_map(example1):
     assert aoc.total_orbits_in_map(example1) == 42
+
+
+def test_get_object_orbited_by_object(example1, example2):
+    assert aoc.get_object_orbited_by_object(example1, "D") == "C"
+    assert aoc.get_object_orbited_by_object(example1, "L") == "K"
+
+    assert aoc.get_object_orbited_by_object(example2, "YOU") == "K"
+    assert aoc.get_object_orbited_by_object(example2, "SAN") == "I"
+
+    assert aoc.get_object_orbited_by_object(example1, "COM") is None
+    assert aoc.get_object_orbited_by_object(example2, "COM") is None
+
+
+def test_orbit_tree(example2):
+    assert aoc.orbit_tree(example2, "YOU") == ["YOU", "K", "J", "E", "D", "C", "B", "COM"]  # 8
+    assert aoc.orbit_tree(example2, "SAN") == ["SAN", "I", "D", "C", "B", "COM"]  # 6
+    assert aoc.orbit_tree(example2, "D") == ["D", "C", "B", "COM"]  # 4
+
+
+def test_find_closest_common_ancestor(example2):
+    assert aoc.find_closest_common_ancestor(example2, "YOU", "SAN") == "D"
+
+
+def test_minimum_orbital_transfers_from_you_to_san(example2):
+    assert aoc.minimum_orbital_transfers_from_you_to_san(example2) == 4
 
 
 # @pytest.mark.skip(reason="Not implemented")
