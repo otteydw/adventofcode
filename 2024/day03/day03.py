0933b6a7680e3533d5a0435d332a850dcb7d984d
Daniel W. Ottey <dan.ottey@boomi.com>
2024-12-02 12:22:53 -0500

    Initial commit for the day.

---
+++ 2024/day03/day03.py
@@ -0,0 +1,38 @@
+import argparse
+import pathlib
+
+
+def parse(puzzle_input):
+    return [line for line in puzzle_input.splitlines()]
+
+
+def part1(data):
+    pass
+
+
+def part2(data):
+    pass
+
+
+def solve(puzzle_input):
+    """Solve the puzzle for the given input."""
+    data = parse(puzzle_input)
+    solve1 = True
+    solve2 = True
+    solution1 = part1(data) if solve1 else None
+    solution2 = part2(data) if solve2 else None
+
+    return solution1, solution2
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Solve Advent of Code puzzles")
+    parser.add_argument("files", nargs="+", help="Input files to process")
+    args = parser.parse_args()
+
+    for path in args.files:
+        print(f"{path}:")
+        puzzle_input = pathlib.Path(path).read_text().strip()
+        solutions = solve(puzzle_input)
+        for solution_number, solution in enumerate(solutions):
+            print(f"Solution {solution_number}: {str(solution)}")
