32e3ca231c33b5ffde74bebe8bb0d6f9fbf44b8f
Daniel W. Ottey <dan.ottey@boomi.com>
2024-12-03 11:15:33 -0500

    2024 Day 3 Part 1 Complete.

--- 2024/day03/day03.py
+++ 2024/day03/day03.py
@@ -1,5 +1,6 @@
 import argparse
 import pathlib
+import re
 
 
 def parse(puzzle_input):
@@ -7,11 +8,35 @@ def parse(puzzle_input):
 
 
 def part1(data):
-    pass
+    value = 0
+    for row in data:
+        muls = find_muls(row)
+        for mul in muls:
+            value += do_mul(mul)
+
+    return value
 
 
 def part2(data):
     pass
+
+
+def do_mul(x: str) -> int:
+    """Given string x in the form 'mul(###,###)', return an integer value of the two values multiplied together."""
+
+    pattern = r"mul\((\d{1,3}),(\d{1,3})\)"
+    match = re.search(pattern, x)
+    return int(match.group(1)) * int(match.group(2))
+
+
+def find_muls(x: str) -> list[str]:
+    """Given string x, return a list of valid mul values in the form
+    ['mul(2,4)', 'mul(23,72)'] etc
+    """
+    # pattern = r"mul\(d{1,3}\,d{1,3}\)"
+    pattern = r"mul\(\d{1,3},\d{1,3}\)"
+    matches = re.findall(pattern, x)
+    return matches
 
 
 def solve(puzzle_input):
--- 2024/day03/test_day03.py
+++ 2024/day03/test_day03.py
@@ -26,10 +26,10 @@ def test_parse_example1(example1):
     assert example1 == ...
 
 
-@pytest.mark.skip(reason="Not implemented")
+# @pytest.mark.skip(reason="Not implemented")
 def test_part1_example1(example1):
     """Test part 1 on example input."""
-    assert aoc.part1(example1) == ...
+    assert aoc.part1(example1) == 161
 
 
 @pytest.mark.skip(reason="Not implemented")
@@ -42,3 +42,12 @@ def test_part2_example1(example1):
 def test_part2_example2(example2):
     """Test part 2 on example input."""
     assert aoc.part2(example2) == ...
+
+
+def test_find_muls(example1):
+    assert aoc.find_muls(example1[0]) == ["mul(2,4)", "mul(5,5)", "mul(11,8)", "mul(8,5)"]
+    assert aoc.find_muls("mul(1234,1)") == []
+
+
+def test_do_mul():
+    assert aoc.do_mul("mul(23,111)") == 2553
