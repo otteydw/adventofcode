b60d69630f1d421ec0f29dbb5bc36f50ccd88f80
Daniel W. Ottey <dan@bluefrogs.us>
2025-12-07 14:09:21 -0500

    2025 Day 02 Initial Template.

---
+++ 2025/day02/day02.py
@@ -0,0 +1,38 @@
+import argparse
+import pathlib
+
+
+def parse(puzzle_input: str) -> list[str]:
+    return [line for line in puzzle_input.splitlines()]
+
+
+def part1(data: list[str]) -> int:  # type: ignore[empty-body]
+    pass
+
+
+def part2(data: list[str]) -> int:  # type: ignore[empty-body]
+    pass
+
+
+def solve(puzzle_input: str) -> tuple[int | None, int | None]:
+    """Solve the puzzle for the given input."""
+    data = parse(puzzle_input)
+    solve1 = True
+    solve2 = True
+    solution1 = part1(data) if solve1 else None
+    solution2 = part2(data) if solve2 else None
+
+    return solution1, solution2
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Solve Advent of Code puzzles")
+    parser.add_argument("files", nargs="+", help="Input files to process")
+    args = parser.parse_args()
+
+    for path in args.files:
+        print(f"{path}:")
+        puzzle_input = pathlib.Path(path).read_text().strip()
+        solutions = solve(puzzle_input)
+        for solution_number, solution in enumerate(solutions, start=1):
+            print(f"Solution {solution_number}: {str(solution)}")
