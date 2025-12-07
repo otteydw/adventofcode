d9b71d083b82eb8f35af71b4275f66d2cc658ee9
Daniel W. Ottey <dan@bluefrogs.us>
2025-12-07 13:57:41 -0500

    2025 Day 1 Complete - messy code.

--- 2025/day01/day01.py
+++ 2025/day01/day01.py
@@ -3,17 +3,35 @@ import pathlib
 
 
 class Safe:
-    def __init__(self, initial_value: int):
+    def __init__(self, initial_value: int, method: str | None = None) -> None:
         self.value = initial_value
+        self.method = method
+        self.zero_counter = 0
+        print(f"The dial starts by pointing at {self.value}.")
 
     def rotate(self, rotation: str) -> None:
         direction = 1
         if rotation[0] == "L":
             direction = -1
-
         distance = int(rotation[1:])
 
-        self.value = (self.value + 100 + (direction * distance)) % 100
+        if not self.method:
+            self.value = (self.value + (direction * distance)) % 100
+        elif self.method == "0x434C49434B":
+            previous_value = self.value
+            complete_revolutions, remaining_distance = divmod(distance, 100)
+            self.value = (self.value + (direction * remaining_distance)) % 100
+            this_zero_counter = complete_revolutions
+            if (
+                previous_value != 0
+                and (direction == -1 and previous_value - remaining_distance <= 0 <= previous_value)
+                or (direction == 1 and previous_value <= 100 <= previous_value + remaining_distance)
+            ):
+                this_zero_counter += 1
+            self.zero_counter += this_zero_counter
+            print(
+                f"The dial is rotated {rotation} to point at {self.value}. During this rotation it points at zero {this_zero_counter} times."
+            )
 
 
 def parse(puzzle_input: str) -> list[str]:
@@ -32,14 +50,20 @@ def part1(data: list[str]) -> int:
     return zero_counter
 
 
-def part2(data: list[str]) -> int:  # type: ignore[empty-body]
-    pass
+def part2(data: list[str]) -> int:
+    safe = Safe(50, method="0x434C49434B")
+    # zero_counter = 0
+    for operation in data:
+        # print(f"{operation=}")
+        safe.rotate(operation)
+        # print(safe.value)
+    return safe.zero_counter
 
 
 def solve(puzzle_input: str) -> tuple[int | None, int | None]:
     """Solve the puzzle for the given input."""
     data = parse(puzzle_input)
-    solve1 = True
+    solve1 = False
     solve2 = True
     solution1 = part1(data) if solve1 else None
     solution2 = part2(data) if solve2 else None
--- 2025/day01/test_day01.py
+++ 2025/day01/test_day01.py
@@ -25,6 +25,47 @@ def test_rotation():
     safe.rotate("L10")
     assert safe.value == 95
 
+    safe = aoc.Safe(5)
+    safe.rotate("L210")
+    assert safe.value == 95
+
+
+def test_rotation_alt_method():
+    safe = aoc.Safe(5, method="0x434C49434B")
+    safe.rotate("L10")
+    assert safe.value == 95
+    assert safe.zero_counter == 1
+
+    safe = aoc.Safe(50, method="0x434C49434B")
+    safe.rotate("R1000")
+    assert safe.value == 50
+    assert safe.zero_counter == 10
+
+    safe = aoc.Safe(50, method="0x434C49434B")
+    safe.rotate("L1000")
+    assert safe.value == 50
+    assert safe.zero_counter == 10
+
+    safe = aoc.Safe(74, method="0x434C49434B")
+    safe.rotate("R28")
+    assert safe.value == 2
+    assert safe.zero_counter == 1
+
+    safe = aoc.Safe(0, method="0x434C49434B")
+    safe.rotate("L5")
+    assert safe.value == 95
+    assert safe.zero_counter == 0
+
+    safe = aoc.Safe(0, method="0x434C49434B")
+    safe.rotate("R14")
+    assert safe.value == 14
+    assert safe.zero_counter == 0
+
+    safe = aoc.Safe(55, method="0x434C49434B")
+    safe.rotate("L55")
+    assert safe.value == 0
+    assert safe.zero_counter == 1
+
 
 @pytest.mark.skip(reason="Not implemented")
 def test_parse_example1(example1):
@@ -38,10 +79,10 @@ def test_part1_example1(example1):
     assert aoc.part1(example1) == 3
 
 
-@pytest.mark.skip(reason="Not implemented")
+# @pytest.mark.skip(reason="Not implemented")
 def test_part2_example1(example1):
     """Test part 2 on example input."""
-    assert aoc.part2(example1) == ...
+    assert aoc.part2(example1) == 6
 
 
 @pytest.mark.skip(reason="Not implemented")
