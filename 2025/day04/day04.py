fcf228168a2fd4edae790958e82dac107e806177
Daniel W. Ottey <dan@bluefrogs.us>
2025-12-08 12:45:06 -0500

    2025 Day 4 Part 1 Complete.

--- 2025/day04/day04.py
+++ 2025/day04/day04.py
@@ -1,13 +1,60 @@
 import argparse
+import itertools
 import pathlib
+
+import numpy as np
 
 
 def parse(puzzle_input: str) -> list[str]:
     return [line for line in puzzle_input.splitlines()]
 
 
-def part1(data: list[str]) -> int:  # type: ignore[empty-body]
-    pass
+def data_to_array(data: list[str]) -> np.ndarray:
+    array = []
+    for row in data:
+        array.append([character for character in row])
+    array = np.array(array)
+    return array
+
+
+def surrounding_coordinates(
+    coordinate: tuple[int, int],
+    inclusive: bool = False,
+    min_coordinate: tuple[int, int] | None = None,
+    max_coordinate: tuple[int, int] | None = None,
+) -> list[tuple[int, int]]:
+    x, y = coordinate
+    possible_x = [x - 1, x, x + 1]
+    possible_y = [y - 1, y, y + 1]
+    possible_coordinates = list(itertools.product(possible_x, possible_y))
+    if not inclusive:
+        possible_coordinates.remove(coordinate)
+
+    if min_coordinate:
+        possible_coordinates = [
+            coord for coord in possible_coordinates if coord[0] >= min_coordinate[0] and coord[1] >= min_coordinate[1]
+        ]
+    if max_coordinate:
+        possible_coordinates = [
+            coord for coord in possible_coordinates if coord[0] <= max_coordinate[0] and coord[1] <= max_coordinate[1]
+        ]
+    return possible_coordinates
+
+
+def part1(data: list[str]) -> int:
+    grid_array = data_to_array(data)
+    shape = grid_array.shape
+    max_coord = (shape[0] - 1, shape[1] - 1)
+    counter = 0
+    for index, value in np.ndenumerate(grid_array):
+        if value == "@":
+            values = []
+            for coord in surrounding_coordinates(index, min_coordinate=(0, 0), max_coordinate=max_coord):
+                if grid_array[coord] == "@":
+                    values.append(coord)
+            if len(values) < 4:
+                counter += 1
+    return counter
 
 
 def part2(data: list[str]) -> int:  # type: ignore[empty-body]
--- 2025/day04/test_day04.py
+++ 2025/day04/test_day04.py
@@ -8,6 +8,38 @@ import pytest
 PUZZLE_DIR = pathlib.Path(__file__).parent
 
 
+def test_surrounding_coordinates():
+    assert aoc.surrounding_coordinates((0, 0)) == [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
+    assert aoc.surrounding_coordinates((10, 10)) == [
+        (9, 9),
+        (9, 10),
+        (9, 11),
+        (10, 9),
+        (10, 11),
+        (11, 9),
+        (11, 10),
+        (11, 11),
+    ]
+    assert aoc.surrounding_coordinates((4, 4)) == [(3, 3), (3, 4), (3, 5), (4, 3), (4, 5), (5, 3), (5, 4), (5, 5)]
+    assert aoc.surrounding_coordinates((4, 4), inclusive=True) == [
+        (3, 3),
+        (3, 4),
+        (3, 5),
+        (4, 3),
+        (4, 4),
+        (4, 5),
+        (5, 3),
+        (5, 4),
+        (5, 5),
+    ]
+    assert aoc.surrounding_coordinates((0, 0), min_coordinate=(0, 0)) == [(0, 1), (1, 0), (1, 1)]
+    assert aoc.surrounding_coordinates((10, 10), max_coordinate=(10, 10)) == [
+        (9, 9),
+        (9, 10),
+        (10, 9),
+    ]
+
+
 @pytest.fixture
 def example1():
     puzzle_input = (PUZZLE_DIR / "example1.txt").read_text().strip()
@@ -26,10 +58,10 @@ def test_parse_example1(example1):
     assert example1 == ...
 
 
-@pytest.mark.skip(reason="Not implemented")
+# @pytest.mark.skip(reason="Not implemented")
 def test_part1_example1(example1):
     """Test part 1 on example input."""
-    assert aoc.part1(example1) == ...
+    assert aoc.part1(example1) == 13
 
 
 @pytest.mark.skip(reason="Not implemented")
