15ebb6a581e78031c2ea6e6b18a52585b28ca3ea
Daniel W. Ottey <dan@bluefrogs.us>
2025-12-08 18:03:21 -0500

    2025 Day 05 Part 1 Complete

--- 2025/day05/day05.py
+++ 2025/day05/day05.py
@@ -6,8 +6,36 @@ def parse(puzzle_input: str) -> list[str]:
     return [line for line in puzzle_input.splitlines()]
 
 
-def part1(data: list[str]) -> int:  # type: ignore[empty-body]
-    pass
+def split_list_on_empty_string(some_list: list[str], split_on: str = "") -> tuple[list[str], list[str]]:
+    split_index = some_list.index(split_on)
+    a = some_list[:split_index]
+    b = some_list[split_index + 1 :]
+    return (a, b)
+
+
+def is_fresh(fresh_ranges: list[tuple[int, int]], id_to_check: int) -> bool:
+    for fresh_range in fresh_ranges:
+        range_min, range_max = fresh_range
+        range_max += 1
+        if id_to_check in range(range_min, range_max):
+            return True
+    return False
+
+
+def part1(data: list[str]) -> int:
+    fresh_ingredient_range_strings, ingredient_id_strings = split_list_on_empty_string(data)
+    fresh_ingredient_ranges = []
+    for id_range in fresh_ingredient_range_strings:
+        range_min_str, range_max_str = id_range.split("-")
+        range_min = int(range_min_str)
+        range_max = int(range_max_str)
+        fresh_ingredient_ranges.append((range_min, range_max))
+    # print(fresh_ingredient_ranges)
+
+    ingredient_ids = [int(id) for id in ingredient_id_strings]
+    # print(ingredient_ids)
+
+    return sum(1 for id in ingredient_ids if is_fresh(fresh_ingredient_ranges, id))
 
 
 def part2(data: list[str]) -> int:  # type: ignore[empty-body]
--- 2025/day05/test_day05.py
+++ 2025/day05/test_day05.py
@@ -8,6 +8,17 @@ import pytest
 PUZZLE_DIR = pathlib.Path(__file__).parent
 
 
+def test_is_fresh():
+    my_ranges = [(3, 5), (10, 14), (16, 20), (12, 18)]
+
+    assert not aoc.is_fresh(my_ranges, 1)
+    assert not aoc.is_fresh(my_ranges, 8)
+    assert not aoc.is_fresh(my_ranges, 32)
+    assert aoc.is_fresh(my_ranges, 5)
+    assert aoc.is_fresh(my_ranges, 11)
+    assert aoc.is_fresh(my_ranges, 17)
+
+
 @pytest.fixture
 def example1():
     puzzle_input = (PUZZLE_DIR / "example1.txt").read_text().strip()
@@ -26,10 +37,9 @@ def test_parse_example1(example1):
     assert example1 == ...
 
 
-@pytest.mark.skip(reason="Not implemented")
 def test_part1_example1(example1):
     """Test part 1 on example input."""
-    assert aoc.part1(example1) == ...
+    assert aoc.part1(example1) == 3
 
 
 @pytest.mark.skip(reason="Not implemented")
