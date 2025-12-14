2793e026a1fa5f77e58606f76c98c73be1f9330d
Daniel W. Ottey <dan@bluefrogs.us>
2025-12-14 09:54:26 -0500

    2025 Day 07 Part 1 Complete

--- 2025/day07/day07.py
+++ 2025/day07/day07.py
@@ -23,10 +23,73 @@ def data_to_array(data: list[str]) -> np.ndarray:
     return array
 
 
+class Coordinate:
+    def __init__(self, value: str) -> None:
+        # self.value = value
+        self.start = value == "S"
+        self.splitter = value == "^"
+        self.beam = self.start
+
+    def __repr__(self) -> str:
+        if self.start:
+            return "S"
+        elif self.splitter:
+            return "^"
+        elif self.beam:
+            return "|"
+        else:
+            return "."
+
+
+# def print_array(array) -> None:
+# for row_num, row in enumerate(array):
+#     for col_num, col in enumerate(row):
+#         if col == "S":
+#             print(col, end="")
+#         elif col == "^":
+#             print(col, end="")
+#         elif beams[row]
+#     print()
+
+# for array_row, beam_row in zip(array, beams):
+#     for array_col, beam_col in zip(array_row, beam_row):
+#         if array_col == "S":
+#             printable=array_col
+#         elif beam_col == "|":
+#             printable="|"
+#         else:
+#             printable=array_col
+#         print(printable, end="")
+#     print()
+
+
 def part1(data: list[str]) -> int:
     array = data_to_array(data)
+    array = np.vectorize(Coordinate, otypes=[object])(array)
     print(array)
-    return 0
+    # beams = np.full_like(array, None, dtype=object)
+    # beams = array.copy()
+    # beams[beams == "S"] = "|"
+    # print_array(array, beams)
+    last_row = len(array) - 1
+    splits = 0
+    for row_num, row in enumerate(array):
+        if row_num == last_row:
+            break
+        for col_num, coord in enumerate(row):
+            # print(f"Checking coordinate {coord}")
+            if coord.beam and not coord.splitter:
+                array[row_num + 1][col_num].beam = True
+            elif coord.splitter and array[row_num - 1][col_num].beam:
+                array[row_num + 1][col_num - 1].beam = True
+                array[row_num + 1][col_num + 1].beam = True
+                splits += 1
+    print()
+    print(array)
+    # print()
+    # print_array(array, beams)
+
+    return splits
 
 
 def part2(data: list[str]) -> int:  # type: ignore[empty-body]
--- 2025/day07/test_day07.py
+++ 2025/day07/test_day07.py
@@ -24,10 +24,10 @@ def test_parse_example1(example1):
     assert example1 == ...
 
 
-@pytest.mark.skip(reason="Not implemented")
+# @pytest.mark.skip(reason="Not implemented")
 def test_part1_example1(example1):
     """Test part 1 on example input."""
-    assert aoc.part1(example1) == ...
+    assert aoc.part1(example1) == 21
 
 
 @pytest.mark.skip(reason="Not implemented")
