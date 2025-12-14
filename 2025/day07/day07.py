44c5e175aeecc0cb414f93f0745a77e96ce14b9e
Daniel W. Ottey <dan@bluefrogs.us>
2025-12-14 08:27:12 -0500

    Data loaded to array

--- 2025/day07/day07.py
+++ 2025/day07/day07.py
@@ -1,6 +1,8 @@
 import argparse
 import pathlib
 
+import numpy as np
+
 
 def load_input(path: pathlib.Path) -> str:
     return pathlib.Path(path).read_text().strip()
@@ -10,8 +12,21 @@ def parse(puzzle_input: str) -> list[str]:
     return [line for line in puzzle_input.splitlines()]
 
 
-def part1(data: list[str]) -> int:  # type: ignore[empty-body]
-    pass
+def data_to_array(data: list[str]) -> np.ndarray:
+    array = []
+    for row in data:
+        columns = []
+        for character in row:
+            columns.append(character)
+        array.append(columns)
+    array = np.array(array)
+    return array
+
+
+def part1(data: list[str]) -> int:
+    array = data_to_array(data)
+    print(array)
+    return 0
 
 
 def part2(data: list[str]) -> int:  # type: ignore[empty-body]
