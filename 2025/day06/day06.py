890187fe8e87fc3ea73b9a7d04cdb0c23f50e31a
Daniel W. Ottey <dan@bluefrogs.us>
2025-12-14 08:10:15 -0500

    Code cleanup

--- 2025/day06/day06.py
+++ 2025/day06/day06.py
@@ -6,6 +6,10 @@ import sys
 import numpy as np
 
 
+def load_input(path: pathlib.Path) -> str:
+    return pathlib.Path(path).read_text()
+
+
 def parse(puzzle_input: str) -> list[str]:
     return [line for line in puzzle_input.splitlines()]
 
@@ -23,38 +27,19 @@ def data_to_array_p2(data: list[str]) -> np.ndarray:
     for row in data:
         columns = []
         for character in row:
-            # print(f"Char is {character}")
             columns.append(character)
         array.append(columns)
-    # from pprint import pprint
-    # pprint(array)
-    print(array)
-    # sys.exit()
-    # for row in array:
-    #     print(f"{len(row)=}")
-
-    # max_length = max(len(row) for row in array)
-    # print(f"{max_length=}")
-    # array = [np.pad(row, (0, max_length - len(row)), 'constant', constant_values=' ') for row in array]
-    # print()
-    # for row in array:
-    #     print(f"{len(row)=}")
     array = np.array(array)
-    # print(array)
-    # sys.exit()
     return array
 
 
 def part1(data: list[str]) -> int:
     grid_array = data_to_array(data)
-    # print(grid_array)
-    # return
     grid_array_t = grid_array.T
     total = 0
     for row in grid_array_t:
         operator = row[-1]
         operands = [int(x) for x in row[:-1]]
-        print(f"{operator=}, {operands=}")
         if operator == "+":
             total += sum(operands)
         elif operator == "*":
@@ -66,26 +51,16 @@ def part1(data: list[str]) -> int:
 
 def part2(data: list[str]) -> int:
     grid_array = data_to_array_p2(data)
-    # print(grid_array)
-    # sys.exit()
+
     operators = np.flip(grid_array[-1])
     operands = np.flipud(grid_array[:-1].T)
 
-    # operators = [operator for operator in operators if operator is not None]
     operators = [operator for operator in operators if operator != " "]
 
-    print()
-    print("Operators")
-    print(operators)
-    print("Operands")
-    print(operands)
-    print()
-    # sys.exit()
     total = 0
     numbers = []
     compute = False
     for row_num, row in enumerate(operands):
-        print(row)
         if (row == " ").all():
             compute = True
         else:
@@ -93,18 +68,14 @@ def part2(data: list[str]) -> int:
             numbers.append(number)
         if row_num == len(operands) - 1:
             compute = True
-        # print(number)
-        # print()
         if compute:
             operator = operators.pop(0)
-            print(f"I will {operator} on {numbers} = ", end="")
             if operator == "+":
                 this_compute = sum(numbers)
             elif operator == "*":
                 this_compute = math.prod(numbers)
             else:
                 sys.exit(f"Unexpected operator! {operator}")
-            print(this_compute)
             total += this_compute
             numbers = []
             compute = False
@@ -114,10 +85,7 @@ def part2(data: list[str]) -> int:
 def solve(puzzle_input: str) -> tuple[int | None, int | None]:
     """Solve the puzzle for the given input."""
     data = parse(puzzle_input)
-    # print(data)
-    # sys.exit()
-    # data=puzzle_input
-    solve1 = False
+    solve1 = True
     solve2 = True
     solution1 = part1(data) if solve1 else None
     solution2 = part2(data) if solve2 else None
@@ -132,8 +100,7 @@ if __name__ == "__main__":
 
     for path in args.files:
         print(f"{path}:")
-        # puzzle_input = pathlib.Path(path).read_text().strip()
-        puzzle_input = pathlib.Path(path).read_text()
+        puzzle_input = load_input(path)
         solutions = solve(puzzle_input)
         for solution_number, solution in enumerate(solutions, start=1):
             print(f"Solution {solution_number}: {str(solution)}")
--- 2025/day06/test_day06.py
+++ 2025/day06/test_day06.py
@@ -1,5 +1,3 @@
-# test_aoc_template.py
-
 import pathlib
 
 import day06 as aoc
@@ -10,47 +8,43 @@ PUZZLE_DIR = pathlib.Path(__file__).parent
 
 @pytest.fixture
 def example1():
-    puzzle_input = (PUZZLE_DIR / "example1.txt").read_text()
+    puzzle_input = aoc.load_input(PUZZLE_DIR / "example1.txt")
     return aoc.parse(puzzle_input)
 
 
 @pytest.fixture
 def example2():
-    puzzle_input = (PUZZLE_DIR / "example2.txt").read_text()
+    puzzle_input = aoc.load_input(PUZZLE_DIR / "example2.txt")
     return aoc.parse(puzzle_input)
 
 
 @pytest.fixture
 def example3():
-    puzzle_input = (PUZZLE_DIR / "example3.txt").read_text()
+    puzzle_input = aoc.load_input(PUZZLE_DIR / "example3.txt")
     return aoc.parse(puzzle_input)
 
 
 @pytest.fixture
 def example4():
-    puzzle_input = (PUZZLE_DIR / "example4.txt").read_text()
+    puzzle_input = aoc.load_input(PUZZLE_DIR / "example4.txt")
     return aoc.parse(puzzle_input)
 
 
-# @pytest.mark.skip(reason="Not implemented")
 def test_parse_example1(example1):
     """Test that input is parsed properly."""
     assert example1 == ["123 328  51 64 ", " 45 64  387 23 ", "  6 98  215 314", "*   +   *   +  "]
 
 
-# @pytest.mark.skip(reason="Not implemented")
 def test_parse_example2(example2):
     """Test that input is parsed properly."""
     assert example2 == ["411", "126", "836", "3  ", "*  "]
 
 
-# @pytest.mark.skip(reason="Not implemented")
 def test_parse_example3(example3):
     """Test that input is parsed properly."""
     assert example3 == ["   9", "  79", " 891", "8392", "+   "]
 
 
-# @pytest.mark.skip(reason="Not implemented")
 def test_parse_example4(example4):
     """Test that input is parsed properly."""
     assert example4 == ["   9 411", "  79 126", " 891 836", "8392 3  ", "+    *  "]
@@ -77,23 +71,14 @@ def test_d2a_example4():
         ["8", "3", "9", "2", " ", "3", " ", " "],
         ["+", " ", " ", " ", " ", "*", " ", " "],
     ]
-    # assert np.array_equal(aoc.data_to_array_p2(original_array), expected_output)
     assert (aoc.data_to_array_p2(original_array) == expected_output).all()
 
 
-@pytest.mark.skip(reason="Not implemented")
 def test_part1_example1(example1):
     """Test part 1 on example input."""
     assert aoc.part1(example1) == 4277556
 
 
-# @pytest.mark.skip(reason="Not implemented")
 def test_part2_example1(example1):
     """Test part 2 on example input."""
     assert aoc.part2(example1) == 3263827
-
-
-@pytest.mark.skip(reason="Not implemented")
-def test_part2_example2(example2):
-    """Test part 2 on example input."""
-    assert aoc.part2(example2) == ...
