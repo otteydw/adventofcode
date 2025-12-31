62bb9f114632b459e88194a7bf4abd06b7614b85
Daniel W. Ottey <dan@bluefrogs.us>
2025-12-31 07:32:30 -0500

    Additional classes and options for filtering problem set based on area requirements.

--- 2025/day12/day12.py
+++ 2025/day12/day12.py
@@ -1,6 +1,5 @@
 import argparse
 import pathlib
-from pprint import pprint
 from typing import Iterable
 
 import numpy as np
@@ -31,6 +30,47 @@ def split_by_seperator(iterable: Iterable[str], seperator: str = "") -> list[lis
     return new_list
 
 
+def array_to_grid(arr2d: np.ndarray) -> str:
+    row_strings = ["".join(map(str, row)) for row in arr2d]
+    clean_string = "\n".join(row_strings)
+    return clean_string
+
+
+class Gift:
+    def __init__(self, data: list[str]) -> None:
+        self.gift_array = data_to_array(data)
+
+    @property
+    def max_point_count(self) -> int:
+        return (self.gift_array == "#").sum()
+
+    def __repr__(self) -> str:
+        return array_to_grid(self.gift_array)
+
+
+class Region:
+    def __init__(self, data: str) -> None:
+        dimensions_string, requirements_string = data.split(": ")
+        width_str, length_str = dimensions_string.split("x")
+        self.width = int(width_str)
+        self.length = int(length_str)
+        self.requirements = [int(x) for x in requirements_string.split(" ")]
+
+    @property
+    def area(self) -> int:
+        return self.width * self.length
+
+    def can_support_gifts(self, gift_list: list[Gift]) -> bool:
+        area_available = self.area
+        area_required = 0
+        for gift_idx, number_of_gifts in enumerate(self.requirements):
+            area_required += gift_list[gift_idx].max_point_count * number_of_gifts
+        return area_required <= area_available
+
+    def __repr__(self) -> str:
+        return f"{self.width}x{self.length} = {self.area} | {self.requirements}"
+
+
 class Problem:
     def __init__(self, data: list[str]) -> None:
         # pprint(data)
@@ -39,39 +79,42 @@ class Problem:
         gift_data = full_data[:-1]
         region_data = full_data[-1]
         # pprint(full_data)
-        pprint(gift_data)
-        pprint(region_data)
+        # pprint(gift_data)
+        # pprint(region_data)
         self._init_gifts(gift_data)
         self._init_regions(region_data)
 
     def _init_gifts(self, data: list[list[str]]) -> None:
-        self.gifts = []
+        self.gifts: list[Gift] = []
 
         for line in data:
             gift_data = line[1:]
-            gift_array = data_to_array(gift_data)
-            self.gifts.append(gift_array)
+            gift = Gift(gift_data)
+            self.gifts.append(gift)
 
     def _init_regions(self, data: list[str]) -> None:
-        self.regions = []
+        self.regions: list[Region] = []
         for line in data:
-            dimensions_string, requirements_string = line.split(": ")
-            width_str, length_str = dimensions_string.split("x")
-            width = int(width_str)
-            length = int(length_str)
-            requirements = [int(x) for x in requirements_string.split(" ")]
-            region = {"width": width, "length": length, "requirements": requirements}
+            region = Region(line)
             self.regions.append(region)
+
+    def report_valid_regions(self) -> None:
+        total_regions = len(self.regions)
+        invalid_regions = 0
+        for region in self.regions:
+            if not region.can_support_gifts(self.gifts):
+                invalid_regions += 1
+        print(f"{total_regions=}, {invalid_regions=}")
 
     def __repr__(self) -> str:
         out = "Gifts\n"
         for idx, gift in enumerate(self.gifts):
-            out += f"{str(idx)}\n"
+            out += f"Gift {str(idx)}, Max Points: {gift.max_point_count}\n"
             out += str(gift)
-            out += "\n"
+            out += "\n\n"
         out += "\nRegions:\n"
         for region in self.regions:
-            out += f"{region['width']}x{region['length']} {region['requirements']}\n"
+            out += f"{region}\n"
         return out
 
 
@@ -80,17 +123,15 @@ def load_input(path: pathlib.Path) -> str:
 
 
 def parse(puzzle_input: str) -> list[str]:
-    # pprint(puzzle_input)
-    # sys.exit()
-    # lines = puzzle_input.splitlines()
-    # pprint(lines)
-    # sys.exit()
+
     return [line for line in puzzle_input.splitlines()]
 
 
 def part1(data: list[str]) -> int:
     problem = Problem(data)
     print(problem)
+    problem.report_valid_regions()
+
     return 0
 
 
