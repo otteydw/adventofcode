dfa8ce37337ee79ae53ab4dd6941c7649025d20e
Daniel W. Ottey <dan@bluefrogs.us>
2026-01-01 11:00:28 -0500

    Add simplified version.

--- 2025/day12/day12_simplified.py
+++ 2025/day12/day12_simplified.py
@@ -3,9 +3,6 @@ import pathlib
 from typing import Iterable
 
 import numpy as np
-from polyomino.board import Rectangle
-from polyomino.constant import MONOMINO
-from polyomino.tileset import any_number_of
 
 
 def data_to_array(data: list[str]) -> np.ndarray:
@@ -47,18 +44,8 @@ class Gift:
     def max_point_count(self) -> int:
         return (self.gift_array == "#").sum()
 
-    @property
-    def as_tuples(self) -> list[tuple[int, int]]:
-        t = []
-        for coord in np.argwhere(self.gift_array == "#"):
-            # print(f"{coord=}")
-            x, y = coord
-            # print(f"{x=}. {y=}")
-            t.append((int(x), int(y)))
-        return t
-
     def __repr__(self) -> str:
-        return array_to_grid(self.gift_array) + "\n" + str(self.as_tuples) + "\n"
+        return array_to_grid(self.gift_array) + "\n"
 
 
 class Region:
@@ -68,7 +55,6 @@ class Region:
         self.width = int(width_str)
         self.length = int(length_str)
         self.requirements = [int(x) for x in requirements_string.split(" ")]
-        self.board = Rectangle(self.width, self.length)
 
     @property
     def area(self) -> int:
@@ -94,9 +80,9 @@ class Region:
 
         max_full_shapes = region_gifts_over_width * region_gifts_over_length
         total_requested_shapes = sum(self.requirements)
-        print(
-            f"For a region {self.width}x{self.length} I can fit {region_gifts_over_width}x{region_gifts_over_length} gifts."
-        )
+        # print(
+        #     f"For a region {self.width}x{self.length} I can fit {region_gifts_over_width}x{region_gifts_over_length} gifts."
+        # )
         return total_requested_shapes <= max_full_shapes
 
     def __repr__(self) -> str:
@@ -125,10 +111,6 @@ class Problem:
             region = Region(line)
             self.regions.append(region)
 
-    # def sort_regions_by_area(self) -> None:
-    #     # Sort regions by area, lowest to highest
-    #     self.regions.sort(key=lambda region: region.area)
-
     def report_valid_regions(self) -> None:
         total_regions = len(self.regions)
         invalid_regions = 0
@@ -141,33 +123,20 @@ class Problem:
         # Remove the regions which cannot support the required gifts just due to area requirements
         self.regions = [region for region in self.regions if region.can_support_gifts(self.gifts)]
 
-    def p1(self) -> int:
+    def p1(self) -> int | None:
+        self.remove_too_small_regions()
+
         solvable = 0
-        for idx, region in enumerate(self.regions, start=1):
-            print(f"Region {idx} - {region}")
+        for region in self.regions:
+            # print(f"Region {idx} - {region}")
             if region.easy_p1():
-                print("Easy solve!")
+                # print("Easy solve!")
                 solvable += 1
                 continue
             else:
-                print("Need further checking...")
-            problem = region.board.tile_with_set(
-                any_number_of([MONOMINO])
-                .and_repeated_exactly(region.requirements[0], self.gifts[0].as_tuples)
-                .and_repeated_exactly(region.requirements[1], self.gifts[1].as_tuples)
-                .and_repeated_exactly(region.requirements[2], self.gifts[2].as_tuples)
-                .and_repeated_exactly(region.requirements[3], self.gifts[3].as_tuples)
-                .and_repeated_exactly(region.requirements[4], self.gifts[4].as_tuples)
-                .and_repeated_exactly(region.requirements[5], self.gifts[5].as_tuples)
-            )
-            # print(f"Solving Region {idx} - {region}")
-            solution = problem.solve()
-            if solution:
-                print("Solved!")
-                # print(solution.display())
-                solvable += 1
-            else:
-                print("No solution")
+                print("At least once region requires more advanced processing. Use `day12.py` instead.")
+                return None
+
         return solvable
 
     def __repr__(self) -> str:
@@ -191,28 +160,17 @@ def parse(puzzle_input: str) -> list[str]:
     return [line for line in puzzle_input.splitlines()]
 
 
-def part1(data: list[str]) -> int:
+def part1(data: list[str]) -> int | None:
     problem = Problem(data)
-    problem.remove_too_small_regions()
-    # problem.sort_regions_by_area()
-    print(problem)
-    problem.report_valid_regions()
     return problem.p1()
 
 
-def part2(data: list[str]) -> int:  # type: ignore[empty-body]
-    pass
-
-
-def solve(puzzle_input: str) -> tuple[int | None, int | None]:
+def solve(puzzle_input: str) -> list[int | None]:
     """Solve the puzzle for the given input."""
     data = parse(puzzle_input)
     solve1 = True
-    solve2 = False  # There was no part 2 today!
     solution1 = part1(data) if solve1 else None
-    solution2 = part2(data) if solve2 else None
-
-    return solution1, solution2
+    return [solution1]
 
 
 if __name__ == "__main__":
