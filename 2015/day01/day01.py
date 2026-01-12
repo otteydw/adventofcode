5bff6d11008ec669d0158e9bf4433c1abf1c254a
Daniel W. Ottey <dan@bluefrogs.us>
2026-01-12 13:12:06 -0500

    2015 Day 1\n- Add back part 1 output.\nType hints

--- 2015/day01/day01.py
+++ 2015/day01/day01.py
@@ -1,35 +1,38 @@
 import os
 
-def find_floor(directions):
+
+def find_floor(directions: str) -> int:
     floor = 0
 
     for direction in directions:
         if direction == "(":
             floor += 1
         elif direction == ")":
-            floor -=1
+            floor -= 1
 
     return floor
 
-def find_basement_position(directions):
+
+def find_basement_position(directions: str) -> int | None:
     floor = 0
 
     for index, direction in enumerate(directions):
         if direction == "(":
             floor += 1
         elif direction == ")":
-            floor -=1
+            floor -= 1
 
         if floor == -1:
             return index + 1
+    return None
 
 
 if __name__ == "__main__":
 
-    input_path = os.path.join(os.path.dirname(__file__),"input.txt")
+    input_path = os.path.join(os.path.dirname(__file__), "input.txt")
 
     with open(input_path) as input_file:
         INPUT = input_file.readline()
 
-    # print(find_floor(INPUT))
-    print(find_basement_position(INPUT))
+    print(f"Part 1: {find_floor(INPUT)}")
+    print(f"Part 2: {find_basement_position(INPUT)}")
