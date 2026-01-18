4f7d5e170f65448786d9428d1d562f26e8fa5307
Daniel W. Ottey <dan@bluefrogs.us>
2026-01-18 11:44:41 -0500

    2015 Day 17 Part 1 Complete

--- 2015/day17/day17.py
+++ 2015/day17/day17.py
@@ -1,5 +1,6 @@
 import argparse
 import pathlib
+from itertools import combinations
 
 
 def load_input(path: pathlib.Path) -> str:
@@ -10,8 +11,17 @@ def parse(puzzle_input: str) -> list[int]:
     return sorted([int(line) for line in puzzle_input.splitlines()])
 
 
-def part1(data: list[int], eggnog: int = 150) -> int:  # type: ignore[empty-body]
-    pass
+def part1(data: list[int], eggnog: int = 150) -> int:
+    # print(data)
+
+    counter = 0
+    for number_in_combination in range(1, len(data) + 1):
+        for combo in combinations(data, number_in_combination):
+            # print(f"{combo=}, {sum(combo)=}")
+            if sum(combo) == eggnog:
+                # print("Increasing counter")
+                counter += 1
+    return counter
 
 
 def part2(data: list[int]) -> int:  # type: ignore[empty-body]
--- 2015/day17/test_day17.py
+++ 2015/day17/test_day17.py
@@ -24,7 +24,7 @@ def test_parse_example1(example1):
     assert example1 == ...
 
 
-@pytest.mark.skip(reason="Not implemented")
+# @pytest.mark.skip(reason="Not implemented")
 def test_part1_example1(example1):
     """Test part 1 on example input."""
     assert aoc.part1(example1, eggnog=25) == 4
