51ba63ad54d4f8daa1b4779a1c4d1a1c74f810a5
Daniel W. Ottey <dan@bluefrogs.us>
2026-01-31 08:00:38 -0500

    Fix adjacent_finder test to import numpy directly

--- helpers/adjacent_finder/test_adjacent_finder.py
+++ helpers/adjacent_finder/test_adjacent_finder.py
@@ -1,11 +1,13 @@
 import unittest
 
+import numpy as np
+
 from . import adjacent_finder
 
 
 class TestAdjacent(unittest.TestCase):
     def test_adjacent_finder(self):
-        npa = adjacent_finder.np.array([[5, 6, 4], [2, 1, 3], [7, 9, 8]])
+        npa = np.array([[5, 6, 4], [2, 1, 3], [7, 9, 8]])
         self.assertEqual(
             adjacent_finder.adjacent_finder(npa, (1, 1)),
             [(0, 0), (0, 1), (0, 2), (1, 0), (1, 2), (2, 0), (2, 1), (2, 2)],
