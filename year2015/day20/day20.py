ada9eaee1191f5f2ec155578888e09caffed4c50
Daniel W. Ottey <dan@bluefrogs.us>
2026-02-08 10:13:18 -0500

    2015 Day 20 Part 2 Complete.

--- year2015/day20/day20.py
+++ year2015/day20/day20.py
@@ -27,15 +27,31 @@ def gifts_delivered_by_elf(elf_number: int, house_number: int) -> int:
 
 
 # @lru_cache(maxsize=None)
-# def gifts_delivered_to_house(house_number: int) -> int:
+# def gifts_delivered_to_house_brute(house_number: int) -> int:
 #     gifts_delivered = 0
 #     for elf in range(1, house_number+1):
-#         gifts_delivered += gifts_delivered_by_elf(elf, house_number)
+#         gifts_delivered += gifts_delivered_by_elf_with_limit(elf, house_number)
 #     return gifts_delivered
 
+# @lru_cache(maxsize=None)
+# def gifts_delivered_by_elf_with_limit(elf_number: int, house_number: int, limit=50) -> int:
+#     gifts_per_delivery = 11
+#     if not house_number % elf_number == 0:
+#         return 0
+#     if house_number > elf_number + limit:
+#         return 0
+#     gifts_delivered_per_house = gifts_per_delivery * elf_number
+#     return gifts_delivered_per_house
 
-def gifts_delivered_to_house(house_number: int) -> int:
-    return 10 * sum(divisors(house_number))
+# def gifts_delivered_to_house(house_number: int) -> int:
+#     return 10 * sum(divisors(house_number))
+
+
+def gifts_delivered_to_house(house_number: int, multiplier: int = 10, limit: None | int = None) -> int:
+    if not limit:
+        return multiplier * sum(divisors(house_number))
+    else:
+        return multiplier * sum(d for d in divisors(house_number) if house_number // d <= limit)
 
 
 def part1(puzzle_input: int) -> int:
@@ -48,14 +64,20 @@ def part1(puzzle_input: int) -> int:
         house += 1
 
 
-def part2(data: int) -> int:  # type: ignore[empty-body]
-    pass
+def part2(puzzle_input: int) -> int:
+    house = 1
+    while True:
+        delivered = gifts_delivered_to_house(house, multiplier=11, limit=50)
+        print(f"House {house} received {delivered} gifts.")
+        if delivered >= puzzle_input:
+            return house
+        house += 1
 
 
 def solve(puzzle_input: int) -> tuple[int | None, int | None]:
     """Solve the puzzle for the given input."""
     # data = parse(puzzle_input)
-    solve1 = True
+    solve1 = False
     solve2 = True
     solution1 = part1(puzzle_input) if solve1 else None
     solution2 = part2(puzzle_input) if solve2 else None
@@ -69,3 +91,5 @@ if __name__ == "__main__":
     solutions = solve(puzzle_input)
     for solution_number, solution in enumerate(solutions, start=1):
         print(f"Solution {solution_number}: {str(solution)}")
+
+# 776160 too high
