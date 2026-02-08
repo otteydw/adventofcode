3d95f0bfdef8589e6d3d89bdcb4f14f52513d136
Daniel W. Ottey <dan@bluefrogs.us>
2026-02-08 09:39:11 -0500

    2015 Day 20 Part 1 Complete but slowish

--- year2015/day20/day20.py
+++ year2015/day20/day20.py
@@ -1,42 +1,71 @@
-import argparse
-import pathlib
+from functools import lru_cache
+
+from sympy import divisors
+
+# House 1 got 10 presents.
+# House 2 got 30 presents.
+# House 3 got 40 presents.
+# House 4 got 70 presents.
+# House 5 got 60 presents.
+# House 6 got 120 presents.
+# House 7 got 80 presents.
+# House 8 got 150 presents.
+# House 9 got 130 presents.
+#
+# While I initially tried to "brute force" the solution, it was very slow.
+# I used ChatGPT to at least determine that this numberical pattern is
+# "a well-known number theory function called the divisor sum function"
 
 
-def load_input(path: pathlib.Path) -> str:
-    return pathlib.Path(path).read_text().strip()
+@lru_cache(maxsize=None)
+def gifts_delivered_by_elf(elf_number: int, house_number: int) -> int:
+    if not house_number % elf_number == 0:
+        return 0
+
+    gifts_delivered_per_house = 10 * elf_number
+    return gifts_delivered_per_house
 
 
-def parse(puzzle_input: str) -> list[str]:
-    return [line for line in puzzle_input.splitlines()]
+# @lru_cache(maxsize=None)
+# def gifts_delivered_to_house(house_number: int) -> int:
+#     gifts_delivered = 0
+#     for elf in range(1, house_number+1):
+#         gifts_delivered += gifts_delivered_by_elf(elf, house_number)
+#     return gifts_delivered
 
 
-def part1(data: list[str]) -> int:  # type: ignore[empty-body]
+def gifts_delivered_to_house(house_number: int) -> int:
+    return 10 * sum(divisors(house_number))
+
+
+def part1(puzzle_input: int) -> int:
+    house = 1
+    while True:
+        delivered = gifts_delivered_to_house(house)
+        print(f"House {house} received {delivered} gifts.")
+        if delivered >= puzzle_input:
+            return house
+        house += 1
+
+
+def part2(data: int) -> int:  # type: ignore[empty-body]
     pass
 
 
-def part2(data: list[str]) -> int:  # type: ignore[empty-body]
-    pass
-
-
-def solve(puzzle_input: str) -> tuple[int | None, int | None]:
+def solve(puzzle_input: int) -> tuple[int | None, int | None]:
     """Solve the puzzle for the given input."""
-    data = parse(puzzle_input)
+    # data = parse(puzzle_input)
     solve1 = True
     solve2 = True
-    solution1 = part1(data) if solve1 else None
-    solution2 = part2(data) if solve2 else None
+    solution1 = part1(puzzle_input) if solve1 else None
+    solution2 = part2(puzzle_input) if solve2 else None
 
     return solution1, solution2
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Solve Advent of Code puzzles")
-    parser.add_argument("files", nargs="+", help="Input files to process")
-    args = parser.parse_args()
 
-    for path in args.files:
-        print(f"{path}:")
-        puzzle_input = load_input(path)
-        solutions = solve(puzzle_input)
-        for solution_number, solution in enumerate(solutions, start=1):
-            print(f"Solution {solution_number}: {str(solution)}")
+    puzzle_input = 29000000
+    solutions = solve(puzzle_input)
+    for solution_number, solution in enumerate(solutions, start=1):
+        print(f"Solution {solution_number}: {str(solution)}")
--- year2015/day20/test_day20.py
+++ year2015/day20/test_day20.py
@@ -7,28 +7,71 @@ from . import day20 as aoc
 PUZZLE_DIR = pathlib.Path(__file__).parent
 
 
-@pytest.fixture
-def example1():
-    puzzle_input = aoc.load_input(PUZZLE_DIR / "example1.txt")
-    return aoc.parse(puzzle_input)
+# @pytest.fixture
+# def example1():
+#     puzzle_input = aoc.load_input(PUZZLE_DIR / "example1.txt")
+#     return aoc.parse(puzzle_input)
 
 
-@pytest.fixture
-def example2():
-    puzzle_input = aoc.load_input(PUZZLE_DIR / "example2.txt")
-    return aoc.parse(puzzle_input)
+# @pytest.fixture
+# def example2():
+#     puzzle_input = aoc.load_input(PUZZLE_DIR / "example2.txt")
+#     return aoc.parse(puzzle_input)
 
 
-@pytest.mark.skip(reason="Not implemented")
-def test_parse_example1(example1):
-    """Test that input is parsed properly."""
-    assert example1 == ...
+# @pytest.mark.skip(reason="Not implemented")
+# def test_parse_example1(example1):
+#     """Test that input is parsed properly."""
+#     assert example1 == ...
 
 
-@pytest.mark.skip(reason="Not implemented")
-def test_part1_example1(example1):
-    """Test part 1 on example input."""
-    assert aoc.part1(example1) == ...
+# @pytest.mark.skip(reason="Not implemented")
+# def test_gifts():
+#     """Test part 1"""
+#     assert aoc.gifts(1) == 10
+#     assert aoc.gifts(2) == 30
+#     assert aoc.gifts(3) == 40
+#     # assert aoc.gifts(4) == 70
+
+
+@pytest.mark.parametrize(
+    "elf, house, expected",
+    [
+        [1, 1, 10],
+        [2, 1, 0],
+        [2, 2, 20],
+        [3, 1, 0],
+        [3, 2, 0],
+        [3, 3, 30],
+        [3, 4, 0],
+        [3, 6, 30],
+    ],
+)
+def test_gifts_delivered_by_elf(elf, house, expected):
+    assert aoc.gifts_delivered_by_elf(elf, house) == expected
+
+
+@pytest.mark.parametrize(
+    "house, expected",
+    [
+        [1, 10],
+        [2, 30],
+        [3, 40],
+        [4, 70],
+        [5, 60],
+        [6, 120],
+        [7, 80],
+        [8, 150],
+        [9, 130],
+    ],
+)
+def test_gifts_delivered_to_house(house, expected):
+    assert aoc.gifts_delivered_to_house(house) == expected
+
+
+def test_part1():
+    assert aoc.part1(30) == 2
+    assert aoc.part1(130) == 8
 
 
 @pytest.mark.skip(reason="Not implemented")
