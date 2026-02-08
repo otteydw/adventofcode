c2d6b365c4e2837de640878bcf8d7c22c89216c2
Daniel W. Ottey <dan@bluefrogs.us>
2026-02-08 07:01:00 -0500

    Functional use of tempfile.

--- year2022/day05/day05.py
+++ year2022/day05/day05.py
@@ -1,4 +1,5 @@
 import os
+import tempfile
 
 import pandas
 
@@ -10,31 +11,36 @@ class Supplies:
     def load_from_file(self, filename):
         tmp_filename = "tmpout.tmp"
         input_path = os.path.join(os.path.dirname(__file__), filename)
-        tmp_output_path = os.path.join(os.path.dirname(__file__), tmp_filename)
+        # tmp_output_path = os.path.join(os.path.dirname(__file__), tmp_filename)
 
-        boxes = []
-        moves = []
-        after_newline = False
+        with tempfile.NamedTemporaryFile(mode="w+t", delete_on_close=False, delete=False) as output_file:
+            tmp_filename = output_file.name
+            print(f"{tmp_filename=}")
 
-        with open(input_path) as input_file:
-            for line in input_file:
-                line_stripped = line.rstrip()
-                if line_stripped == "":
-                    after_newline = True
-                    continue
-                if after_newline:
-                    moves.append(line_stripped)
+            boxes = []
+            moves = []
+            after_newline = False
 
-                else:
-                    boxes.append(line_stripped)
+            with open(input_path) as input_file:
+                for line in input_file:
+                    line_stripped = line.rstrip()
+                    if line_stripped == "":
+                        after_newline = True
+                        continue
+                    if after_newline:
+                        moves.append(line_stripped)
 
-        boxes.reverse()
-        with open(tmp_output_path, "w") as output_file:
+                    else:
+                        boxes.append(line_stripped)
+
+            boxes.reverse()
+            # with open(tmp_output_path, "w") as output_file:
             for line in boxes:
                 output_file.write(line + "\n")
 
-        boxes_df = pandas.DataFrame.transpose(pandas.read_fwf(tmp_filename)).fillna("")
-        os.remove(tmp_filename)
+            output_file.flush()
+            boxes_df = pandas.DataFrame.transpose(pandas.read_fwf(tmp_filename)).fillna("")
+            # os.remove(tmp_filename)
         boxes_df = boxes_df.replace(r"\[|\]", "", regex=True)
         boxes_list = boxes_df.values.tolist()
 
